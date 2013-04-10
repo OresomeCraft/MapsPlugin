c49045dd884b95122e8eab4b8efbf3873bccdb73
Zach De Koning <zach.de.koning@gmail.com>
2013-04-10 23:19:09 +1000

    Only fire Nuketown special effects if the current map is Nuketown

--- src/com/maps/classes/Nuketown.java
+++ src/com/maps/classes/Nuketown.java
@@ -227,14 +227,16 @@ public class Nuketown extends BattleMap implements MapInterface, Listener {
 
     @EventHandler
     public void arrowCollide(ProjectileHitEvent e) {
-	if (e.getEntity() instanceof Arrow) {
-	    Arrow a = (Arrow) e.getEntity();
-	    Location loc1 = a.getLocation();
-	    List<Entity> nearby = a.getNearbyEntities(1, 1, 1);
-	    for (Entity ent : nearby) {
-		if (ent instanceof Player) {
-		    Player p = (Player) ent;
-		    ent.playEffect(EntityEffect.WOLF_SMOKE);
+	if (e.getEntity().getWorld().equals(name)) {
+	    if (e.getEntity() instanceof Arrow) {
+		Arrow a = (Arrow) e.getEntity();
+		Location loc1 = a.getLocation();
+		List<Entity> nearby = a.getNearbyEntities(1, 1, 1);
+		for (Entity ent : nearby) {
+		    if (ent instanceof Player) {
+			Player p = (Player) ent;
+			ent.playEffect(EntityEffect.WOLF_SMOKE);
+		    }
 		}
 	    }
 	}
@@ -242,14 +244,16 @@ public class Nuketown extends BattleMap implements MapInterface, Listener {
 
     @EventHandler
     public void onInteract(PlayerInteractEvent e) {
-	Player p = e.getPlayer();
-	ItemStack iih = p.getItemInHand();
-	Material m = iih.getType();
-	if (m == Material.BOW) {
-	    if (e.getAction() == Action.RIGHT_CLICK_AIR
-		    || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-		p.getWorld().playEffect(p.getLocation(),
-			Effect.MOBSPAWNER_FLAMES, 0, 50);
+	if (e.getPlayer().getWorld().equals(name)) {
+	    Player p = e.getPlayer();
+	    ItemStack iih = p.getItemInHand();
+	    Material m = iih.getType();
+	    if (m == Material.BOW) {
+		if (e.getAction() == Action.RIGHT_CLICK_AIR
+			|| e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+		    p.getWorld().playEffect(p.getLocation(),
+			    Effect.MOBSPAWNER_FLAMES, 0, 50);
+		}
 	    }
 	}
     }
