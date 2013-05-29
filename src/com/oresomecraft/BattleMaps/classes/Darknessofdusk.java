fc96a23bc903905ab5e4f2b9cf14b18cfd1af424
R3creat3 <reicyMC@gmail.com>
2013-05-30 09:39:31 +1000

    Try to stop the hiding on Darkness Of Dusk...
    Use an asynchronous task that runs every 30 seconds to check where
    humans are.

--- src/com/oresomecraft/BattleMaps/classes/Darknessofdusk.java
+++ src/com/oresomecraft/BattleMaps/classes/Darknessofdusk.java
@@ -25,169 +25,186 @@ import org.bukkit.inventory.meta.LeatherArmorMeta;
 
 public class Darknessofdusk extends BattleMap implements MapInterface, Listener {
 
-    OresomeBattlesMaps plugin;
-    public Darknessofdusk(OresomeBattlesMaps pl) {
-        super(pl);
-        plugin = pl;
-    }
+	OresomeBattlesMaps plugin;
+	public Darknessofdusk(OresomeBattlesMaps pl) {
+		super(pl);
+		plugin = pl;
+	}
 
-    // Spawn lists. (Don't change!)
-    public ArrayList<Location> redSpawns = new ArrayList<Location>();
-    public ArrayList<Location> blueSpawns = new ArrayList<Location>();
-    public ArrayList<Location> FFASpawns = new ArrayList<Location>();
+	// Spawn lists. (Don't change!)
+	public ArrayList<Location> redSpawns = new ArrayList<Location>();
+	public ArrayList<Location> blueSpawns = new ArrayList<Location>();
+	public ArrayList<Location> FFASpawns = new ArrayList<Location>();
 
-    // Map details
-    String name = "dusk";
-    String fullName = "Darkness of Dusk";
-    String creators = "R3creat3, xannallax33, dinner1111 and pepsidawg00";
-    Gamemode[] modes = {Gamemode.INFECTION};
-    //Map download link: N/A
+	// Map details
+	String name = "dusk";
+	String fullName = "Darkness of Dusk";
+	String creators = "R3creat3, xannallax33, dinner1111 and pepsidawg00";
+	Gamemode[] modes = {Gamemode.INFECTION};
+	//Map download link: N/A
 
-    @EventHandler(priority = EventPriority.NORMAL)
-    public void readyMap(ReadyMapsEvent event) {
-        addMap(name);
-        readyTDMSpawns();
-        readyFFASpawns();
-        setGamemodes(name, modes);
-        addCreators(name, creators);
-        setFullName(name, fullName);
-    }
-    //Tdm isn't enabled on this, don't need to do spawns.
-    public void readyTDMSpawns() {
-        World w = Bukkit.getServer().getWorld(name);
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void readyMap(ReadyMapsEvent event) {
+		addMap(name);
+		readyTDMSpawns();
+		readyFFASpawns();
+		setGamemodes(name, modes);
+		addCreators(name, creators);
+		setFullName(name, fullName);
+		stopHidingNoobs();
+	}
+	//Tdm isn't enabled on this, don't need to do spawns.
+	public void readyTDMSpawns() {
+		World w = Bukkit.getServer().getWorld(name);
 
-        Location redSpawn = new Location(w, 0, 99, 27, 2, 0);
-        Location blueSpawn = new Location(w, -9, 110, -20, 0, 0);
+		Location redSpawn = new Location(w, 0, 99, 27, 2, 0);
+		Location blueSpawn = new Location(w, -9, 110, -20, 0, 0);
 
-        redSpawns.add(redSpawn);
-        blueSpawns.add(blueSpawn);
-        setRedSpawns(name, redSpawns);
-        setBlueSpawns(name, blueSpawns);
-    }
+		redSpawns.add(redSpawn);
+		blueSpawns.add(blueSpawn);
+		setRedSpawns(name, redSpawns);
+		setBlueSpawns(name, blueSpawns);
+	}
 
-    public void readyFFASpawns() {
+	public void readyFFASpawns() {
 
-        World w = Bukkit.getServer().getWorld(name);
-        FFASpawns.add(new Location(w, 62, 6, 23));
-        FFASpawns.add(new Location(w, 62, 6, 149));
-        FFASpawns.add(new Location(w, -46, 6, 147));
-        FFASpawns.add(new Location(w, -48, 6, 44));
+		World w = Bukkit.getServer().getWorld(name);
+		FFASpawns.add(new Location(w, 62, 6, 23));
+		FFASpawns.add(new Location(w, 62, 6, 149));
+		FFASpawns.add(new Location(w, -46, 6, 147));
+		FFASpawns.add(new Location(w, -48, 6, 44));
 
-        setFFASpawns(name, FFASpawns);
-    }
+		setFFASpawns(name, FFASpawns);
+	}
 
-    @EventHandler(priority = EventPriority.NORMAL)
-    public void applyInventory(InventoryEvent event) {
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void applyInventory(InventoryEvent event) {
 
-        String par = event.getMessage();
-        Player p = event.getPlayer();
-        Inventory i = p.getInventory();
-        if (par.equalsIgnoreCase(name)) {
-            clearInv(p);
+		String par = event.getMessage();
+		Player p = event.getPlayer();
+		Inventory i = p.getInventory();
+		if (par.equalsIgnoreCase(name)) {
+			clearInv(p);
 
-            ItemStack HEALTH_POTION = new ItemStack(Material.POTION, 1, (short) 16373);
-            ItemStack STEAK = new ItemStack(Material.COOKED_BEEF, 1);
-            ItemStack BOW = new ItemStack(Material.BOW, 1);
-            ItemStack ARROWS = new ItemStack(Material.ARROW, 64);
-            ItemStack LEATHER_HELMET = new ItemStack(Material.LEATHER_HELMET, 1);
-            ItemStack IRON_CHESTPLATE = new ItemStack(Material.IRON_CHESTPLATE, 1);
-            ItemStack GOLD_PANTS = new ItemStack(Material.GOLD_LEGGINGS, 1);
-            ItemStack DIAMOND_BOOTS = new ItemStack(Material.DIAMOND_BOOTS, 1);
-            ItemStack STONE_SWORD = new ItemStack(Material.STONE_SWORD, 1);
+			ItemStack HEALTH_POTION = new ItemStack(Material.POTION, 1, (short) 16373);
+			ItemStack STEAK = new ItemStack(Material.COOKED_BEEF, 1);
+			ItemStack BOW = new ItemStack(Material.BOW, 1);
+			ItemStack ARROWS = new ItemStack(Material.ARROW, 64);
+			ItemStack LEATHER_HELMET = new ItemStack(Material.LEATHER_HELMET, 1);
+			ItemStack IRON_CHESTPLATE = new ItemStack(Material.IRON_CHESTPLATE, 1);
+			ItemStack GOLD_PANTS = new ItemStack(Material.IRON_LEGGINGS, 1);
+			ItemStack DIAMOND_BOOTS = new ItemStack(Material.IRON_BOOTS, 1);
+			ItemStack STONE_SWORD = new ItemStack(Material.IRON_SWORD, 1);
 
-            if (TDM.isBlue(p.getName())) {
+			if (TDM.isBlue(p.getName())) {
 
-                LeatherArmorMeta helmetMeta = (LeatherArmorMeta) LEATHER_HELMET.getItemMeta();
-                helmetMeta.setColor(Color.BLUE);
-                LEATHER_HELMET.setItemMeta(helmetMeta);
+				LeatherArmorMeta helmetMeta = (LeatherArmorMeta) LEATHER_HELMET.getItemMeta();
+				helmetMeta.setColor(Color.BLUE);
+				LEATHER_HELMET.setItemMeta(helmetMeta);
 
-            }
+			}
 
-            if (TDM.isRed(p.getName())) {
+			if (TDM.isRed(p.getName())) {
 
-                LeatherArmorMeta helmetMeta = (LeatherArmorMeta) LEATHER_HELMET.getItemMeta();
-                helmetMeta.setColor(Color.RED);
-                LEATHER_HELMET.setItemMeta(helmetMeta);
+				LeatherArmorMeta helmetMeta = (LeatherArmorMeta) LEATHER_HELMET.getItemMeta();
+				helmetMeta.setColor(Color.RED);
+				LEATHER_HELMET.setItemMeta(helmetMeta);
 
-            }
+			}
 
-            p.getInventory().setBoots(DIAMOND_BOOTS);
-            p.getInventory().setLeggings(GOLD_PANTS);
-            p.getInventory().setChestplate(IRON_CHESTPLATE);
-            p.getInventory().setHelmet(LEATHER_HELMET);
+			p.getInventory().setBoots(DIAMOND_BOOTS);
+			p.getInventory().setLeggings(GOLD_PANTS);
+			p.getInventory().setChestplate(IRON_CHESTPLATE);
+			p.getInventory().setHelmet(LEATHER_HELMET);
 
-            i.setItem(0, STONE_SWORD);
-            i.setItem(1, BOW);
-            i.setItem(2, STEAK);
-            i.setItem(3, HEALTH_POTION);
-            i.setItem(4, ARROWS);
+			i.setItem(0, STONE_SWORD);
+			i.setItem(1, BOW);
+			i.setItem(2, STEAK);
+			i.setItem(3, HEALTH_POTION);
+			i.setItem(4, ARROWS);
 
-        }
-    }
+		}
+	}
 
-    @EventHandler(priority = EventPriority.NORMAL)
-    public void clearSpawns(ClearSpawnsEvent event) {
-        redSpawns.clear();
-        blueSpawns.clear();
-        FFASpawns.clear();
-    }
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void clearSpawns(ClearSpawnsEvent event) {
+		redSpawns.clear();
+		blueSpawns.clear();
+		FFASpawns.clear();
+	}
 
-    // Region. (Top corner block and bottom corner block.
-    // Top left corner.
-    public int x1 = 115;
-    public int y1 = 57;
-    public int z1 = -14;
+	// Region. (Top corner block and bottom corner block.
+	// Top left corner.
+	public int x1 = 115;
+	public int y1 = 57;
+	public int z1 = -14;
 
-    //Bottom right corner.
-    public int x2 = -88;
-    public int y2 = 0;
-    public int z2 = 191;
+	//Bottom right corner.
+	public int x2 = -88;
+	public int y2 = 0;
+	public int z2 = 191;
 
-    // Getting the region
-    public boolean contains(Location loc, int x1, int x2, int y1,
-                            int y2, int z1, int z2) {
-        int bottomCornerX = x1 < x2 ? x1 : x2;
-        int bottomCornerZ = z1 < z2 ? z1 : z2;
-        int topCornerX = x1 > x2 ? x1 : x2;
-        int topCornerZ = z1 > z2 ? z1 : z2;
-        int bottomCornerY = y1 < y2 ? y1 : y2;
-        int topCornerY = y1 > y2 ? y1 : y2;
-        if (loc.getX() >= bottomCornerX && loc.getX() <= topCornerX) {
-            if (loc.getZ() >= bottomCornerZ && loc.getZ() <= topCornerZ) {
-                if (loc.getY() >= bottomCornerY && loc.getY() <= topCornerY) {
-                    return true;
-                }
-            }
-        }
-        return false;
+	// Getting the region
+	public boolean contains(Location loc, int x1, int x2, int y1,
+			int y2, int z1, int z2) {
+		int bottomCornerX = x1 < x2 ? x1 : x2;
+		int bottomCornerZ = z1 < z2 ? z1 : z2;
+		int topCornerX = x1 > x2 ? x1 : x2;
+		int topCornerZ = z1 > z2 ? z1 : z2;
+		int bottomCornerY = y1 < y2 ? y1 : y2;
+		int topCornerY = y1 > y2 ? y1 : y2;
+		if (loc.getX() >= bottomCornerX && loc.getX() <= topCornerX) {
+			if (loc.getZ() >= bottomCornerZ && loc.getZ() <= topCornerZ) {
+				if (loc.getY() >= bottomCornerY && loc.getY() <= topCornerY) {
+					return true;
+				}
+			}
+		}
+		return false;
 
-    }
+	}
 
-    // Code to prevent block breaking.
-    @EventHandler(priority = EventPriority.NORMAL)
-    public void protection(BlockBreakEvent event) {
+	// Code to prevent block breaking.
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void protection(BlockBreakEvent event) {
 
-        Block b = event.getBlock();
-        Location loc = b.getLocation();
+		Block b = event.getBlock();
+		Location loc = b.getLocation();
 
-        if (loc.getWorld().getName().equals(name)) {
+		if (loc.getWorld().getName().equals(name)) {
 
-            event.setCancelled(true);
-        }
+			event.setCancelled(true);
+		}
 
-    }
+	}
 
-    // Code to prevent block placing.
-    @EventHandler(priority = EventPriority.NORMAL)
-    public void protection1(BlockPlaceEvent event) {
+	// Code to prevent block placing.
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void protection1(BlockPlaceEvent event) {
 
-        Block b = event.getBlock();
-        Location loc = b.getLocation();
+		Block b = event.getBlock();
+		Location loc = b.getLocation();
 
-        if (loc.getWorld().getName().equals(name)) {
+		if (loc.getWorld().getName().equals(name)) {
 
-            event.setCancelled(true);
+			event.setCancelled(true);
 
-        }
-    }
+		}
+	}
+	int Delay;
+	public void stopHidingNoobs(){
+		Delay = Bukkit.getScheduler().scheduleAsyncRepeatingTask(plugin, new Runnable() {
+			public void run() {
+				if(!(OresomeBattles.activeArena.equals(name)){
+					return;
+				}
+				for (Player p : Bukkit.getOnlinePlayers()){
+					if(Infection.isHuman(p.getName())){
+						p.getWorld().strikeLightningEffect(p.getLocation());
+					}
+				}
+				plugin.getServer().broadcastMessage(ChatColor.RED+"[OresomeBattles] Lightning strikes where there are humans!");
+			}
+		}, 20*30, 0);
+	}
 }
