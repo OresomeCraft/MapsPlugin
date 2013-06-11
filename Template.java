a221c884f7cd474c000887770ef406e2211415eb
Zach De Koning <zach.de.koning@gmail.com>
2013-06-11 22:41:23 +1000

    Remove some code from the template

--- Template.java
+++ Template.java
@@ -225,21 +225,4 @@ public class Template extends BattleMap implements MapInterface, Listener {
 
     }
 
-    @EventHandler(priority = EventPriority.NORMAL)
-    public void protection(BlockBreakEvent event) {
-        Block b = event.getBlock();
-        int mat = b.getTypeId();
-        Location loc = b.getLocation();
-        if (loc.getWorld().getName().equals(name)) {
-            if (contains(loc, x1, x2, y1, y2, z1, z2) == true) {
-
-                if (mat == 43 || mat == 44 || mat == 35 || mat == 42
-                        || mat == 49 || mat == 123 || mat == 69 || mat == 124) {
-
-                    event.setCancelled(true);
-                }
-            }
-        }
-    }
-
 }
