351f4ad3a5400f245bd248e35fcd5c41a98a5756
R3creat3 <reicyMC@gmail.com>
2013-07-09 19:25:09 +1000

    Fix Clash Of Clay bugs

--- src/com/oresomecraft/BattleMaps/classes/ClashOfClay.java
+++ src/com/oresomecraft/BattleMaps/classes/ClashOfClay.java
@@ -87,7 +87,8 @@ public class ClashOfClay extends BattleMap implements IBattleMap, Listener {
 
 		Location redSpawn = new Location(w, 0, 99, 27, 2, 0);
 		Location blueSpawn = new Location(w, -9, 110, -20, 0, 0);
-
+		FFASpawns.add(blueSpawn);
+		FFASpawns.add(redSpawn);
 		setFFASpawns(name, FFASpawns);
 	}
 
@@ -100,7 +101,7 @@ public class ClashOfClay extends BattleMap implements IBattleMap, Listener {
 		if (par.equalsIgnoreCase(name)) {
 			clearInv(p);
 
-			ItemStack WOODEN_SWORD = new ItemStack(Material.WOOD_SWORD, 1, (short) 16373);
+			ItemStack WOODEN_SWORD = new ItemStack(Material.WOOD_SWORD, 1, (short) -16373);
 			ItemStack BOW = new ItemStack(Material.BOW, 1);
 			ItemStack IRON_PICKAXE = new ItemStack(Material.IRON_PICKAXE, 1);
 			ItemStack PUMPKIN_PIE = new ItemStack(Material.PUMPKIN_PIE, 5);
@@ -108,7 +109,6 @@ public class ClashOfClay extends BattleMap implements IBattleMap, Listener {
 			ItemStack LEATHER_CHESTPLATE = new ItemStack(Material.LEATHER_CHESTPLATE, 1);
 			ItemStack LEATHER_HELMET = new ItemStack(Material.LEATHER_HELMET, 1);
 			ItemStack TORCH = new ItemStack(Material.TORCH, 16);
-			ItemStack LEATHER = new ItemStack(Material.LEATHER, 1);
 			ItemStack ARROW = new ItemStack(Material.ARROW, 1);
 
 			p.getInventory().setChestplate(LEATHER_CHESTPLATE);
@@ -121,7 +121,6 @@ public class ClashOfClay extends BattleMap implements IBattleMap, Listener {
 			i.setItem(3, PUMPKIN_PIE);
 			i.setItem(4, BLUE_STAINED_CLAY);
 			i.setItem(5, TORCH);
-			i.setItem(6, LEATHER);
 			i.setItem(27, ARROW);
 
 		}
@@ -164,186 +163,4 @@ public class ClashOfClay extends BattleMap implements IBattleMap, Listener {
 		return false;
 
 	}
-
-	// Code to prevent block breaking.
-	@EventHandler(priority = EventPriority.NORMAL)
-	public void protection(BlockBreakEvent event) {
-
-		Block b = event.getBlock();
-		Location loc = b.getLocation();
-
-		if (loc.getWorld().getName().equals(name)) {
-
-			event.setCancelled(true);
-		}
-
-	}
-
-	// Code to prevent block placing.
-	@EventHandler(priority = EventPriority.NORMAL)
-	public void protection1(BlockPlaceEvent event) {
-
-		Block b = event.getBlock();
-		Location loc = b.getLocation();
-
-		if (loc.getWorld().getName().equals(name)) {
-
-			event.setCancelled(true);
-
-		}
-
-	}
-	java.util.Map<Player, Block> placer = new java.util.HashMap<Player, Block>();
-	java.util.Map<Block, Player> placerB = new java.util.HashMap<Block, Player>();
-
-	@SuppressWarnings("deprecation")
-	@EventHandler(priority = EventPriority.NORMAL)
-	public void c4(org.bukkit.event.player.PlayerInteractEvent event) {
-
-		Player p = event.getPlayer();
-		Location loc = p.getLocation();
-		org.bukkit.event.block.Action a = event.getAction();
-		ItemStack i = p.getItemInHand();
-		Material tool = i.getType();
-		org.bukkit.block.BlockFace face = event.getBlockFace();
-		Inventory inv = p.getInventory();
-		String name = p.getName();
-
-		if (!GameUtils.isSpectator(name)) {
-
-			if (contains(loc, x1, x2, y1, y2, z1, z2)) {
-				if (tool == Material.LEATHER) {
-
-					if (a == org.bukkit.event.block.Action.RIGHT_CLICK_BLOCK) {
-
-						Block b = event.getClickedBlock();
-						Block place = b.getRelative(face);
-
-						if (!placerB.containsValue(p)) {
-
-							place.setType(Material.BRICK);
-							p.playSound(loc, org.bukkit.Sound.STEP_GRAVEL, 1, 1);
-
-							placer.put(p, place);
-							placerB.put(place, p);
-
-						}
-					}
-
-				}
-
-				if (a == org.bukkit.event.block.Action.RIGHT_CLICK_AIR) {
-
-					if (placer.containsKey(p) && placerB.containsValue(p)) {
-
-						Block charge = placer.get(p);
-						Location chargeloc = charge.getLocation();
-						World w = charge.getWorld();
-						p.playSound(loc, org.bukkit.Sound.CLICK, 1, 1);
-						w.createExplosion(chargeloc, 4);
-
-						ItemStack LEATHER = new ItemStack(Material.LEATHER, 1);
-						org.bukkit.inventory.meta.ItemMeta leather = LEATHER.getItemMeta();
-						leather.setDisplayName(org.bukkit.ChatColor.BLUE + "C4");
-						LEATHER.setItemMeta(leather);
-
-						inv.removeItem(LEATHER);
-						p.updateInventory();
-						placer.remove(p);
-						placerB.remove(charge);
-
-					}
-				}
-			}
-		}
-	}
-
-	@SuppressWarnings("deprecation")
-	@EventHandler(priority = EventPriority.NORMAL)
-	public void c4premature(org.bukkit.event.entity.ProjectileHitEvent event) {
-		org.bukkit.entity.Projectile p = event.getEntity();
-		World w = p.getWorld();
-		World world = Bukkit.getWorld(name);
-		if (event.getEntity().getWorld().getName().equals(name)) {
-			if (p instanceof org.bukkit.entity.Arrow) {
-				world.playEffect(p.getLocation(), org.bukkit.Effect.STEP_SOUND, 10);
-			}
-		}
-
-		Location loc = p.getLocation();
-		Block b = loc.getBlock();
-		Block bD = b.getRelative(org.bukkit.block.BlockFace.DOWN);
-		Block bU = b.getRelative(org.bukkit.block.BlockFace.UP);
-		Block bE = b.getRelative(org.bukkit.block.BlockFace.EAST);
-		Block bW = b.getRelative(org.bukkit.block.BlockFace.WEST);
-		Block bN = b.getRelative(org.bukkit.block.BlockFace.NORTH);
-		Block bS = b.getRelative(org.bukkit.block.BlockFace.SOUTH);
-
-		ItemStack LEATHER = new ItemStack(Material.LEATHER, 1);
-		org.bukkit.inventory.meta.ItemMeta leather = LEATHER.getItemMeta();
-		leather.setDisplayName(org.bukkit.ChatColor.BLUE + "C4");
-		LEATHER.setItemMeta(leather);
-
-		if (contains(loc, x1, x2, y1, y2, z1, z2)) {
-
-			if (placer.containsValue(b)) {
-				Player own = placerB.get(b);
-				Inventory i = own.getInventory();
-				i.removeItem(LEATHER);
-				own.updateInventory();
-				w.createExplosion(loc, 4);
-				placerB.remove(b);
-
-			} else if (placer.containsValue(bD)) {
-				Player own = placerB.get(bD);
-				Inventory i = own.getInventory();
-				i.removeItem(LEATHER);
-				own.updateInventory();
-				w.createExplosion(loc, 4);
-				placerB.remove(bD);
-
-			} else if (placer.containsValue(bU)) {
-				Player own = placerB.get(bU);
-				Inventory i = own.getInventory();
-				i.removeItem(LEATHER);
-				own.updateInventory();
-				w.createExplosion(loc, 4);
-				placerB.remove(bU);
-
-			} else if (placer.containsValue(bE)) {
-				Player own = placerB.get(bE);
-				Inventory i = own.getInventory();
-				i.removeItem(LEATHER);
-				own.updateInventory();
-				w.createExplosion(loc, 4);
-				placerB.remove(bE);
-
-			} else if (placer.containsValue(bW)) {
-				Player own = placerB.get(bW);
-				// Inventory i = own.getInventory();
-				// i.removeItem(LEATHER);
-				own.updateInventory();
-				w.createExplosion(loc, 4);
-				placerB.remove(bW);
-
-			} else if (placer.containsValue(bN)) {
-				Player own = placerB.get(bN);
-				Inventory i = own.getInventory();
-				i.removeItem(LEATHER);
-				own.updateInventory();
-				w.createExplosion(loc, 4);
-				placerB.remove(bN);
-
-			} else if (placer.containsValue(bS)) {
-				Player own = placerB.get(bS);
-				// Inventory i = own.getInventory();
-				// i.removeItem(LEATHER);
-				own.updateInventory();
-				w.createExplosion(loc, 4);
-				placerB.remove(bS);
-
-			}
-		}
-	}
-
 }
