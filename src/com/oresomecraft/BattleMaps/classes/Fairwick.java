6efe54bcc4935d08e8c9c9a531418bea023ac4c0
R3creat3 <reicyMC@gmail.com>
2013-07-11 19:09:22 +1000

    Prevent confusion on Fairwick with block drops

--- src/com/oresomecraft/BattleMaps/classes/Fairwick.java
+++ src/com/oresomecraft/BattleMaps/classes/Fairwick.java
@@ -14,6 +14,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.event.world.WorldLoadEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -29,156 +30,166 @@ public class Fairwick extends BattleMap implements IBattleMap, Listener {
 
     OresomeBattlesMaps plugin;
 
-    public Fairwick(OresomeBattlesMaps pl) {
-        super(pl);
-        plugin = pl;
-    }
+	public Fairwick(OresomeBattlesMaps pl) {
+		super(pl);
+		plugin = pl;
+	}
 
-    // Spawn lists. (Don't change!)
-    public ArrayList<Location> redSpawns = new ArrayList<Location>();
-    public ArrayList<Location> blueSpawns = new ArrayList<Location>();
-    public ArrayList<Location> FFASpawns = new ArrayList<Location>();
+	// Spawn lists. (Don't change!)
+	public ArrayList<Location> redSpawns = new ArrayList<Location>();
+	public ArrayList<Location> blueSpawns = new ArrayList<Location>();
+	public ArrayList<Location> FFASpawns = new ArrayList<Location>();
 
-    // Map details
-    String name = "fairwick";
-    String fullName = "Fairwick Village";
-    String creators = "R3creat3, ninsai and zachoz";
-    Gamemode[] modes = {Gamemode.CTF};
-    //Map download link: N/A
+	// Map details
+	String name = "fairwick";
+	String fullName = "Fairwick Village";
+	String creators = "R3creat3, ninsai and zachoz";
+	Gamemode[] modes = {Gamemode.CTF};
+	//Map download link: N/A
 
-    @EventHandler(priority = EventPriority.NORMAL)
-    public void readyMap(ReadyMapsEvent event) { // Internal - Do not change
-        addMap(name);
-        addCreators(name, creators);
-        setFullName(name, fullName);
-        setGamemodes(name, modes);
-    }
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void readyMap(ReadyMapsEvent event) { // Internal - Do not change
+		addMap(name);
+		addCreators(name, creators);
+		setFullName(name, fullName);
+		setGamemodes(name, modes);
+	}
 
-    @EventHandler
-    public void setSpawns(WorldLoadEvent event) { // Internal - Do not change
-        if (event.getWorld().getName().equals(name)) {
-            readyTDMSpawns();
-            readyFFASpawns();
-        }
-    }
+	@EventHandler
+	public void setSpawns(WorldLoadEvent event) { // Internal - Do not change
+		if (event.getWorld().getName().equals(name)) {
+			readyTDMSpawns();
+			readyFFASpawns();
+		}
+	}
 
-    public void readyTDMSpawns() {
-        World w = Bukkit.getServer().getWorld(name);
+	public void readyTDMSpawns() {
+		World w = Bukkit.getServer().getWorld(name);
 
-        redSpawns.add(new Location(w, 72, 73, 1));
-        blueSpawns.add(new Location(w, 72, 73, 133));
+		redSpawns.add(new Location(w, 72, 73, 1));
+		blueSpawns.add(new Location(w, 72, 73, 133));
 
-        setRedSpawns(name, redSpawns);
-        setBlueSpawns(name, blueSpawns);
+		setRedSpawns(name, redSpawns);
+		setBlueSpawns(name, blueSpawns);
 
-        Location redFlag = new Location(w, 72, 74, -2);
-        Location blueFlag = new Location(w, 72, 74, 136);
-        setCTFFlags(name, redFlag, blueFlag);
-    }
+		Location redFlag = new Location(w, 72, 74, -2);
+		Location blueFlag = new Location(w, 72, 74, 136);
+		setCTFFlags(name, redFlag, blueFlag);
+	}
 
-    public void readyFFASpawns() {
+	public void readyFFASpawns() {
 
-        World w = Bukkit.getServer().getWorld(name);
-        FFASpawns.add(new Location(w, 72, 73, 1));
-        FFASpawns.add(new Location(w, 72, 73, 133));
+		World w = Bukkit.getServer().getWorld(name);
+		FFASpawns.add(new Location(w, 72, 73, 1));
+		FFASpawns.add(new Location(w, 72, 73, 133));
 
-        setFFASpawns(name, FFASpawns);
-    }
+		setFFASpawns(name, FFASpawns);
+	}
 
-    @EventHandler(priority = EventPriority.NORMAL)
-    public void applyInventory(InventoryEvent event) {
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void applyInventory(InventoryEvent event) {
 
-        String par = event.getMessage();
-        Player p = event.getPlayer();
-        Inventory i = p.getInventory();
-        if (par.equalsIgnoreCase(name)) {
-            clearInv(p);
+		String par = event.getMessage();
+		Player p = event.getPlayer();
+		Inventory i = p.getInventory();
+		if (par.equalsIgnoreCase(name)) {
+			clearInv(p);
 
-            ItemStack HEALTH_POTION = new ItemStack(Material.GOLDEN_APPLE, 3);
-            ItemStack STEAK = new ItemStack(Material.COOKED_BEEF, 3);
-            ItemStack BOW = new ItemStack(Material.BOW, 1);
-            ItemStack ARROWS = new ItemStack(Material.ARROW, 64);
-            ItemStack IRON_HELMET = new ItemStack(Material.IRON_HELMET, 1);
-            ItemStack IRON_CHESTPLATE = new ItemStack(Material.IRON_CHESTPLATE, 1);
-            ItemStack IRON_PANTS = new ItemStack(Material.IRON_LEGGINGS, 1);
-            ItemStack IRON_BOOTS = new ItemStack(Material.IRON_BOOTS, 1);
-            ItemStack IRON_SWORD = new ItemStack(Material.IRON_SWORD, 1);
+			ItemStack HEALTH_POTION = new ItemStack(Material.GOLDEN_APPLE, 3);
+			ItemStack STEAK = new ItemStack(Material.COOKED_BEEF, 3);
+			ItemStack BOW = new ItemStack(Material.BOW, 1);
+			ItemStack ARROWS = new ItemStack(Material.ARROW, 64);
+			ItemStack IRON_HELMET = new ItemStack(Material.IRON_HELMET, 1);
+			ItemStack IRON_CHESTPLATE = new ItemStack(Material.IRON_CHESTPLATE, 1);
+			ItemStack IRON_PANTS = new ItemStack(Material.IRON_LEGGINGS, 1);
+			ItemStack IRON_BOOTS = new ItemStack(Material.IRON_BOOTS, 1);
+			ItemStack IRON_SWORD = new ItemStack(Material.IRON_SWORD, 1);
 
-            p.getInventory().setBoots(IRON_BOOTS);
-            p.getInventory().setLeggings(IRON_PANTS);
-            p.getInventory().setChestplate(IRON_CHESTPLATE);
-            p.getInventory().setHelmet(IRON_HELMET);
+			p.getInventory().setBoots(IRON_BOOTS);
+			p.getInventory().setLeggings(IRON_PANTS);
+			p.getInventory().setChestplate(IRON_CHESTPLATE);
+			p.getInventory().setHelmet(IRON_HELMET);
 
-            i.setItem(0, IRON_SWORD);
-            i.setItem(1, BOW);
-            i.setItem(2, STEAK);
-            i.setItem(3, HEALTH_POTION);
-            i.setItem(4, ARROWS);
+			i.setItem(0, IRON_SWORD);
+			i.setItem(1, BOW);
+			i.setItem(2, STEAK);
+			i.setItem(3, HEALTH_POTION);
+			i.setItem(4, ARROWS);
 
-        }
-    }
+		}
+	}
 
-    @EventHandler(priority = EventPriority.NORMAL)
-    public void clearSpawns(ClearSpawnsEvent event) {
-        redSpawns.clear();
-        blueSpawns.clear();
-        FFASpawns.clear();
-    }
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void clearSpawns(ClearSpawnsEvent event) {
+		redSpawns.clear();
+		blueSpawns.clear();
+		FFASpawns.clear();
+	}
 
-    // Region. (Top corner block and bottom corner block.
-    // Top left corner.
-    public int x1 = -15;
-    public int y1 = 75;
-    public int z1 = 231;
+	// Region. (Top corner block and bottom corner block.
+			// Top left corner.
+			public int x1 = -15;
+	public int y1 = 75;
+	public int z1 = 231;
 
-    //Bottom right corner.
-    public int x2 = 129;
-    public int y2 = 184;
-    public int z2 = -10;
+	//Bottom right corner.
+	public int x2 = 129;
+	public int y2 = 184;
+	public int z2 = -10;
 
-    // Getting the region
-    public boolean contains(Location loc, int x1, int x2, int y1,
-                            int y2, int z1, int z2) {
-        int bottomCornerX = x1 < x2 ? x1 : x2;
-        int bottomCornerZ = z1 < z2 ? z1 : z2;
-        int topCornerX = x1 > x2 ? x1 : x2;
-        int topCornerZ = z1 > z2 ? z1 : z2;
-        int bottomCornerY = y1 < y2 ? y1 : y2;
-        int topCornerY = y1 > y2 ? y1 : y2;
-        if (loc.getX() >= bottomCornerX && loc.getX() <= topCornerX) {
-            if (loc.getZ() >= bottomCornerZ && loc.getZ() <= topCornerZ) {
-                if (loc.getY() >= bottomCornerY && loc.getY() <= topCornerY) {
-                    return true;
-                }
-            }
-        }
-        return false;
+	// Getting the region
+	public boolean contains(Location loc, int x1, int x2, int y1,
+			int y2, int z1, int z2) {
+		int bottomCornerX = x1 < x2 ? x1 : x2;
+		int bottomCornerZ = z1 < z2 ? z1 : z2;
+		int topCornerX = x1 > x2 ? x1 : x2;
+		int topCornerZ = z1 > z2 ? z1 : z2;
+		int bottomCornerY = y1 < y2 ? y1 : y2;
+		int topCornerY = y1 > y2 ? y1 : y2;
+		if (loc.getX() >= bottomCornerX && loc.getX() <= topCornerX) {
+			if (loc.getZ() >= bottomCornerZ && loc.getZ() <= topCornerZ) {
+				if (loc.getY() >= bottomCornerY && loc.getY() <= topCornerY) {
+					return true;
+				}
+			}
+		}
+		return false;
 
-    }
+	}
 
-    // Code to prevent block breaking.
-    @EventHandler(priority = EventPriority.NORMAL)
-    public void protection1(BlockBreakEvent event) {
+	// Code to prevent block breaking.
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void protection1(BlockBreakEvent event) {
 
-        Block b = event.getBlock();
-        Location loc = b.getLocation();
+		Block b = event.getBlock();
+		Location loc = b.getLocation();
 
-        if (loc.getWorld().getName().equals(name)) {
-            event.setCancelled(true);
-        }
+		if (loc.getWorld().getName().equals(name)) {
+			event.setCancelled(true);
+		}
 
-    }
+	}
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void protection1(PlayerPickupItemEvent event) {
 
-    // Code to prevent block placing.
-    @EventHandler(priority = EventPriority.NORMAL)
-    public void protection1(BlockPlaceEvent event) {
+		if (event.getPlayer().getWorld().getName().equals(name)) {
+			if(event.getItem().getItemStack().getType().equals(Material.WOOL)){
+				event.setCancelled(true);
+				event.getItem().remove();
+			}
+		}
+	}
 
-        Block b = event.getBlock();
-        Location loc = b.getLocation();
+	// Code to prevent block placing.
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void protection1(BlockPlaceEvent event) {
 
-        if (loc.getWorld().getName().equals(name)) {
-            event.setCancelled(true);
-        }
+		Block b = event.getBlock();
+		Location loc = b.getLocation();
 
-    }
+		if (loc.getWorld().getName().equals(name)) {
+			event.setCancelled(true);
+		}
+
+	}
 }
