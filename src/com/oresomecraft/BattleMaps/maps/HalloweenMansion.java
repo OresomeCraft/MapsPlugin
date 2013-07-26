3b765303341da3b2c2190499611643bb56356995
psgs <fpigerre@gmail.com>
2013-07-26 20:49:51 +1000

    Add Halloween Mansion
    @R3creat3 Yaw is relevant, as it defines the viewpoint of the player
    when spawning. Pitch is less relevant.

---
+++ src/com/oresomecraft/BattleMaps/maps/HalloweenMansion.java
@@ -0,0 +1,166 @@
+package com.oresomecraft.BattleMaps.maps;
+
+import com.oresomecraft.BattleMaps.BattleMap;
+import org.bukkit.*;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockBurnEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.BlockSpreadEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import com.oresomecraft.BattleMaps.*;
+import com.oresomecraft.OresomeBattles.*;
+import com.oresomecraft.OresomeBattles.events.InventoryEvent;
+
+public class HalloweenMansion extends BattleMap implements IBattleMap, Listener {
+
+    public HalloweenMansion() {
+        super.initiate(this);
+        setDetails(name, fullName, creators, modes);
+    }
+
+    // Map details
+    String name = "halloween";
+    String fullName = "Halloween Mansion";
+    String creators = "bumsonfire";
+    Gamemode[] modes = {Gamemode.TDM, Gamemode.FFA};
+
+    public void readyTDMSpawns() {
+        World w = Bukkit.getServer().getWorld(name);
+
+        Location redSpawn = new Location(w, -230, 78, 17, 0, 0);
+        Location blueSpawn = new Location(w, -267, 85, 87, -145, 0);
+
+        redSpawns.add(redSpawn);
+        redSpawns.add(new Location(w, -255, 79, 46, 0, 0));
+        redSpawns.add(new Location(w, -269, 83, 25, -68, 0));
+
+        blueSpawns.add(blueSpawn);
+        blueSpawns.add(new Location(w, -214, 83, 92, 150, 0));
+        blueSpawns.add(new Location(w, -258, 80, 64, 90, 0));
+
+        setRedSpawns(name, redSpawns);
+        setBlueSpawns(name, blueSpawns);
+    }
+
+    public void readyFFASpawns() {
+
+        World w = Bukkit.getServer().getWorld(name);
+
+        FFASpawns.add(new Location(w, -230, 78, 17, 0, 0));
+        FFASpawns.add(new Location(w, -267, 85, 87, -145, 0));
+        FFASpawns.add(new Location(w, -255, 79, 46, 0, 0));
+        FFASpawns.add(new Location(w, -269, 83, 25, -68, 0));
+        FFASpawns.add(new Location(w, -214, 83, 92, 150, 0));
+        FFASpawns.add(new Location(w, -258, 80, 64, 90, 0));
+
+        setFFASpawns(name, FFASpawns);
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void applyInventory(InventoryEvent event) {
+
+        String par = event.getMessage();
+        final Player p = event.getPlayer();
+        Inventory i = p.getInventory();
+        if (par.equalsIgnoreCase(name)) {
+            clearInv(p);
+
+            ItemStack HEALTH_POTION = new ItemStack(Material.POTION, 1, (short) 16373);
+            ItemStack STEAK = new ItemStack(Material.COOKED_BEEF, 3);
+            ItemStack BOW = new ItemStack(Material.BOW, 1);
+            ItemStack ARROWS = new ItemStack(Material.ARROW, 64);
+            ItemStack IRON_HELMET = new ItemStack(Material.IRON_HELMET, 1);
+            ItemStack IRON_CHESTPLATE = new ItemStack(Material.IRON_CHESTPLATE, 1);
+            ItemStack IRON_PANTS = new ItemStack(Material.IRON_LEGGINGS, 1);
+            ItemStack IRON_BOOTS = new ItemStack(Material.IRON_BOOTS, 1);
+            ItemStack IRON_SWORD = new ItemStack(Material.IRON_SWORD, 1);
+            ItemStack EXP = new ItemStack(Material.EXP_BOTTLE, 5);
+
+            p.getInventory().setBoots(IRON_BOOTS);
+            p.getInventory().setLeggings(IRON_PANTS);
+            p.getInventory().setChestplate(IRON_CHESTPLATE);
+            p.getInventory().setHelmet(IRON_HELMET);
+
+            i.setItem(0, IRON_SWORD);
+            i.setItem(1, BOW);
+            i.setItem(2, STEAK);
+            i.setItem(3, HEALTH_POTION);
+            i.setItem(4, ARROWS);
+            i.setItem(5, EXP);
+
+        }
+
+    }
+
+    // Top left corner.
+    public int x1 = -8;
+    public int y1 = 164;
+    public int z1 = 16;
+
+    //Bottom right corner.
+    public int x2 = -85;
+    public int y2 = 62;
+    public int z2 = 99;
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void preventblockbreak(BlockBreakEvent event) {
+
+        Block b = event.getBlock();
+        Location loc = b.getLocation();
+
+        if (loc.getWorld().getName().equals(name)) {
+
+            event.setCancelled(true);
+        }
+
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void preventblockplace(BlockPlaceEvent event) {
+
+        Block b = event.getBlock();
+        Location loc = b.getLocation();
+
+        if (loc.getWorld().getName().equals(name)) {
+
+            event.setCancelled(true);
+
+        }
+
+    }
+
+    @EventHandler
+    public void preventspread(BlockSpreadEvent event) {
+
+        if (event.getBlock().getWorld().getName().equals(name)) {
+            if ((event.getBlock().getTypeId() != 2) || (event.getBlock().getTypeId() != 3)) {
+
+                event.setCancelled(true);
+
+            }
+        }
+
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void preventburn(BlockBurnEvent event) {
+
+        Block b = event.getBlock();
+        Location loc = b.getLocation();
+
+        if (loc.getWorld().getName().equals(name)) {
+
+            event.setCancelled(true);
+
+        }
+
+    }
+
+}
