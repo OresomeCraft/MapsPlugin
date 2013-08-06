60735d6dad71dde341a69d17d44486d1462d25ea
R3creat3 <reicyMC@gmail.com>
2013-08-06 15:00:08 +1000

    This stupid firework still sends you flying, fix plz

--- src/com/oresomecraft/BattleMaps/maps/Zoned.java
+++ src/com/oresomecraft/BattleMaps/maps/Zoned.java
@@ -104,7 +104,7 @@ public class Zoned extends BattleMap implements IBattleMap, Listener {
             if (event.getAction().equals(Action.RIGHT_CLICK_AIR) || event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
                 if (p.getItemInHand().getType() == Material.FIREWORK) {
                     p.getInventory().removeItem(new ItemStack(Material.FIREWORK, 1));
-                    p.setVelocity(new Vector(-2, 2, 2));
+                    p.setVelocity(new Vector(0, 1.4, 0));
                 }
             }
         }
