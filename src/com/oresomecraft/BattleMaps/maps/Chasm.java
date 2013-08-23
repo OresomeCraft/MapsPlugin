0fd6b78ef32c5512f03d4ffce3a0647fc5b9116c
R3creat3 <reicyMC@gmail.com>
2013-08-23 12:41:47 +1000

    Add The Chasm

---
+++ src/com/oresomecraft/BattleMaps/maps/Chasm.java
@@ -0,0 +1,74 @@
+package com.oresomecraft.BattleMaps.maps;
+
+import org.bukkit.*;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.event.*;
+import org.bukkit.inventory.*;
+
+import com.oresomecraft.BattleMaps.*;
+import com.oresomecraft.OresomeBattles.api.*;
+
+public class Chasm extends BattleMap implements IBattleMap, Listener {
+
+    public Chasm() {
+        super.initiate(this, name, fullName, creators, modes);
+        setAllowBuild(false);
+        disableDrops(new Material[]{Material.LEATHER_HELMET, Material.STONE_SWORD});
+        lockTime("day");
+    }
+
+    String name = "chasm";
+    String fullName = "The Chasm";
+    String creators = "R3creat3, danielschroeder, Spantezian and DynaDavidson";
+    Gamemode[] modes = {Gamemode.CTF, Gamemode.TDM};
+
+    public void readyTDMSpawns() {
+
+        Location redSpawn = new Location(w, 9, 92, -155);
+        Location blueSpawn = new Location(w, 93, 92, 28);
+
+        redSpawns.add(redSpawn);
+        blueSpawns.add(blueSpawn);
+
+        Location redFlag = new Location(w, 48, 88, -132);
+        Location blueFlag = new Location(w, 54, 88, 4);
+        setCTFFlags(name, redFlag, blueFlag);
+    }
+
+    public void readyFFASpawns() {
+        Location redSpawn = new Location(w, 9, 92, -155);
+        Location blueSpawn = new Location(w, 93, 92, 28);
+        FFASpawns.add(redSpawn);
+        FFASpawns.add(blueSpawn);
+    }
+
+    public void applyInventory(final BattlePlayer p) {
+        Inventory i = p.getInventory();
+
+        ItemStack HEALTH = new ItemStack(Material.GOLDEN_APPLE, 1);
+        ItemStack STEAK = new ItemStack(Material.COOKED_BEEF, 3);
+        ItemStack BOW = new ItemStack(Material.BOW, 1);
+        ItemStack ARROWS = new ItemStack(Material.ARROW, 64);
+        ItemStack LEATHER_HELMET = new ItemStack(Material.LEATHER_HELMET, 1);
+        LEATHER_HELMET.addEnchantment(Enchantment.PROTECTION_EXPLOSIONS, 4);
+        ItemStack LEATHER_CHESTPLATE = new ItemStack(Material.LEATHER_CHESTPLATE, 1);
+        ItemStack LEATHER_PANTS = new ItemStack(Material.LEATHER_LEGGINGS, 1);
+        ItemStack LEATHER_BOOTS = new ItemStack(Material.LEATHER_BOOTS, 1);
+        ItemStack STONE_SWORD = new ItemStack(Material.STONE_SWORD, 1);
+
+        InvUtils.colourArmourAccordingToTeam(p, new ItemStack[]{LEATHER_CHESTPLATE, LEATHER_PANTS, LEATHER_HELMET, LEATHER_BOOTS});
+
+        p.getInventory().setBoots(LEATHER_BOOTS);
+        p.getInventory().setLeggings(LEATHER_PANTS);
+        p.getInventory().setChestplate(LEATHER_CHESTPLATE);
+        p.getInventory().setHelmet(LEATHER_HELMET);
+
+        i.setItem(0, STONE_SWORD);
+        i.setItem(1, BOW);
+        i.setItem(2, STEAK);
+        i.setItem(3, HEALTH);
+        i.setItem(10, ARROWS);
+
+    }
+
+}
