8da4fae141c2b6feefae9c578513055b351e53f4
R3creat3 <reicyMC@gmail.com>
2013-08-23 15:30:41 +1000

    Add callumary to Rainbow Road's creator list, update inventory

--- src/com/oresomecraft/BattleMaps/maps/Rainbow.java
+++ src/com/oresomecraft/BattleMaps/maps/Rainbow.java
@@ -11,12 +11,12 @@ public class Rainbow extends BattleMap implements IBattleMap, Listener {
 
     public Rainbow() {
         super.initiate(this, name, fullName, creators, modes);
-        disableDrops(new Material[]{Material.LEATHER_HELMET, Material.STONE_SWORD});
+        disableDrops(new Material[]{Material.LEATHER_HELMET, Material.STONE_SWORD, Material.GOLD_LEGGINGS, Material.GOLD_BOOTS});
     }
 
     String name = "rainbow";
     String fullName = "Rainbow Road";
-    String creators = "R3creat3, MintyPvP, JacquiRose, DynaDavidson, AlphaMinecraft91, simonwilson123 and _Moist";
+    String creators = "R3creat3, MintyPvP, JacquiRose, DynaDavidson, AlphaMinecraft91, simonwilson123, callumary and _Moist";
     Gamemode[] modes = {Gamemode.TDM};
 
     public void readyTDMSpawns() {
@@ -43,6 +43,8 @@ public class Rainbow extends BattleMap implements IBattleMap, Listener {
         ItemStack ARROWS = new ItemStack(Material.ARROW, 64);
         ItemStack LEATHER_HELMET = new ItemStack(Material.LEATHER_HELMET, 1);
         ItemStack LEATHER_CHESTPLATE = new ItemStack(Material.LEATHER_CHESTPLATE, 1);
+        ItemStack GOLD_LEGGINGS = new ItemStack(Material.GOLD_LEGGINGS, 1);
+        ItemStack GOLD_BOOTS = new ItemStack(Material.GOLD_BOOTS, 1);
         ItemStack STONE_SWORD = new ItemStack(Material.STONE_SWORD, 1);
         ItemStack PICK = new ItemStack(Material.IRON_PICKAXE, 1);
 
@@ -50,6 +52,8 @@ public class Rainbow extends BattleMap implements IBattleMap, Listener {
         
         p.getInventory().setChestplate(LEATHER_CHESTPLATE);
         p.getInventory().setHelmet(LEATHER_HELMET);
+        p.getInventory().setBoots(GOLD_BOOTS);
+        p.getInventory().setLeggings(GOLD_LEGGINGS);
 
         i.setItem(0, STONE_SWORD);
         i.setItem(1, BOW);
