f30d530db30a60e23a44b07951bb41b92a830bec
Zach de Koning <zach.de.koning@gmail.com>
2013-09-03 16:37:22 +1000

    Actually fix the rouge method

--- src/com/oresomecraft/BattleMaps/maps/Fairwick.java
+++ src/com/oresomecraft/BattleMaps/maps/Fairwick.java
@@ -74,15 +74,12 @@ public class Fairwick extends BattleMap implements IBattleMap, Listener {
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
-        if(event.getBlock().getLocation().getWorld().getName() == name){
-            if (event.getBlock().getType().getId() != 102) event.setCancelled(true);
-        }
+        if (event.getBlock().getLocation().getWorld().getName().equals(name) && event.getBlock().getType().getId() != 102)
+            event.setCancelled(true);
     }
 
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
-        if(event.getBlock().getLocation().getWorld().getName() == name){
-            event.setCancelled(true);
-        }
+        if (event.getBlock().getLocation().getWorld().getName().equals(name) event.setCancelled(true);
     }
 }
