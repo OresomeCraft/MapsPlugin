ccca78e1fb117f6bbdd54e14a50012f756afaa89
R3creat3 <reicymc@gmail.com>
2013-09-08 14:42:58 +1000

    Give a 'Spy Watch' to Fairwick

--- src/com/oresomecraft/BattleMaps/maps/Fairwick.java
+++ src/com/oresomecraft/BattleMaps/maps/Fairwick.java
@@ -1,11 +1,20 @@
 package com.oresomecraft.BattleMaps.maps;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.*;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.*;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import com.oresomecraft.BattleMaps.*;
 import com.oresomecraft.OresomeBattles.api.*;
@@ -40,6 +49,14 @@ public class Fairwick extends BattleMap implements IBattleMap, Listener {
 
         ItemStack HEALTH_POTION = new ItemStack(Material.GOLDEN_APPLE, 1);
         //Limit golden apples to one, since absorption was added.
+        ItemStack SPY_WATCH = new ItemStack(Material.WATCH, 1);
+        ItemMeta s = SPY_WATCH.getItemMeta();
+        s.setDisplayName(ChatColor.BLUE + "Spy Watch");
+
+        List<String> sLore = new ArrayList<String>();
+        sLore.add(org.bukkit.ChatColor.BLUE + "Interact with this watch to go temporarily invisible!");
+        s.setLore(sLore);
+        SPY_WATCH.setItemMeta(s);
         ItemStack STEAK = new ItemStack(Material.COOKED_BEEF, 3);
         ItemStack BOW = new ItemStack(Material.BOW, 1);
         ItemStack ARROWS = new ItemStack(Material.ARROW, 64);
@@ -73,6 +90,32 @@ public class Fairwick extends BattleMap implements IBattleMap, Listener {
     public int y2 = 184;
     public int z2 = -10;
 
+    @EventHandler
+    public void onSpyWatchInteract(PlayerInteractEvent event){
+        if(!event.getPlayer().getWorld().getName().equals(name)) return;
+        Player p = event.getPlayer();
+        Action a = event.getAction();
+        if(a == Action.RIGHT_CLICK_AIR || a == Action.RIGHT_CLICK_BLOCK){
+            if(p.getItemInHand().getType() == Material.WATCH){
+                p.getInventory().remove(new ItemStack(Material.WATCH, 1));
+                p.getInventory().setHelmet(new ItemStack(Material.AIR, 1));
+                p.getInventory().setChestplate(new ItemStack(Material.AIR, 1));
+                p.getInventory().setLeggings(new ItemStack(Material.AIR, 1));
+                p.getInventory().setBoots(new ItemStack(Material.AIR, 1));
+                p.getInventory().remove(new ItemStack(Material.IRON_BOOTS));
+                p.getInventory().remove(new ItemStack(Material.IRON_LEGGINGS));
+                p.getInventory().remove(new ItemStack(Material.IRON_HELMET));
+                p.getInventory().remove(new ItemStack(Material.LEATHER_CHESTPLATE));
+                p.getInventory().addItem(new ItemStack(Material.IRON_BOOTS));
+                p.getInventory().addItem(new ItemStack(Material.IRON_LEGGINGS));
+                p.getInventory().addItem(new ItemStack(Material.IRON_HELMET));
+                ItemStack LEATHER_CHESTPLATE = new ItemStack(Material.LEATHER_CHESTPLATE, 1);
+                InvUtils.colourArmourAccordingToTeam(BattlePlayer.getBattlePlayer(p), new ItemStack[]{LEATHER_CHESTPLATE});
+                p.getInventory().addItem(LEATHER_CHESTPLATE);
+                p.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 15*20, 0));
+            }
+        }
+    }
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
         if (event.getBlock().getLocation().getWorld().getName().equals(name) && event.getBlock().getType().getId() != 102)
