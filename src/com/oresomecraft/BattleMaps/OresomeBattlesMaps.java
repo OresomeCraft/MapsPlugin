7a3452bea456c2f7dd782b55cb216d5bbeedbbc3
Zach de Koning <zach.de.koning@gmail.com>
2013-09-12 23:06:46 +1000

    Simplify map class loading
    No more needing to manually create a new instance of each class. Just
    drop the class config file in and it will do the rest for you.

--- src/com/oresomecraft/BattleMaps/OresomeBattlesMaps.java
+++ src/com/oresomecraft/BattleMaps/OresomeBattlesMaps.java
@@ -1,9 +1,10 @@
 package com.oresomecraft.BattleMaps;
 
+import java.util.Set;
 import java.util.logging.Logger;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import com.oresomecraft.BattleMaps.maps.*;
+import org.reflections.Reflections;
 
 /**
  * OresomeBattlesMaps | Component for OresomeBattles
@@ -12,8 +13,25 @@ import com.oresomecraft.BattleMaps.maps.*;
  */
 public class OresomeBattlesMaps extends JavaPlugin {
 
-    public final Logger logger = Logger.getLogger("Minecraft");
+    public static final Logger logger = Logger.getLogger("Minecraft");
     protected static OresomeBattlesMaps plugin;
+
+    public void onEnable() {
+        loadMaps("com.oresomecraft.BattleMaps.maps");
+    }
+
+    public static void loadMaps(String packageName) {
+        Reflections reflections = new Reflections(packageName);
+        Set<Class<? extends BattleMap>> classes = reflections.getSubTypesOf(BattleMap.class);
+        for (Class<? extends BattleMap> clazz : classes) {
+            try {
+                BattleMap battleMap = clazz.newInstance();
+            } catch (Exception e) {
+                logger.severe("Unable to load map: " + clazz.getName());
+                e.printStackTrace();
+            }
+        }
+    }
 
     public OresomeBattlesMaps() {
         plugin = this;
@@ -21,76 +39,6 @@ public class OresomeBattlesMaps extends JavaPlugin {
 
     public void onDisable() {
 
-    }
-
-    public void onEnable() {
-        loadMaps();
-    }
-
-    public void loadMaps() {
-        new Apollo();
-        new Zoned();
-        new Mantle();
-        new Roseley();
-        new Perro();
-        new Fairwick();
-        new Wartown();
-        new Spire();
-        new Xenon();
-        new Mansion();
-        new Hypno();
-        new Arctic();
-        new Mutiny();
-        new Nuketown();
-        new Terminal();
-        new Sandtrap();
-        new Towers();
-        new Hartshire();
-        new Skyislands();
-        new Fosscrest();
-        new Solitude();
-        new Suburban();
-        new MutinyII();
-        new Battlement();
-        new Insanity();
-        new Carnival();
-        new Sub();
-        new Deepcaverns();
-        new Darknessofdusk();
-        new Docks();
-        new Spaceships();
-        new Relation();
-        new Raid();
-        new Voidsflag();
-        new DimensionalWar();
-        new Mayhem();
-        new BurnFirePort();
-        new ClashOfClay();
-        new Elements();
-        new GibsonDesertBattles();
-        new WarTrauma();
-        new ClashOfClayII();
-        new Sunshine();
-        new Electricity();
-        new HauntedHouse();
-        new Crater();
-        new Gladiator();
-        new ElementsII();
-        new TheBowl();
-        new Simplex();
-        new Treetop();
-        new BiomeBattle();
-        new Yuzkave();
-        new Alpines();
-        new Rainbow();
-        new Equator();
-        new Chasm();
-        new TowerHill();
-        new DesertCastle();
-        new Oasis();
-        new SkyFights();
-        new TelluricPath();
-        new ClashOfClayIII();
     }
 
     public static OresomeBattlesMaps getInstance() {
