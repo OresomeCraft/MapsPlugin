262bea6391eb238e743138bbe083a81593fcec9d
psgs <fpigerre@gmail.com>
2013-09-21 17:09:39 +1000

    Add Yaw to distortion spawns

--- src/com/oresomecraft/BattleMaps/maps/Distortion.java
+++ src/com/oresomecraft/BattleMaps/maps/Distortion.java
@@ -14,7 +14,6 @@ import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.world.WorldLoadEvent;
 import org.bukkit.inventory.*;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.potion.*;
 import org.bukkit.util.Vector;
 
 import com.oresomecraft.BattleMaps.*;
@@ -43,21 +42,30 @@ public class Distortion extends BattleMap implements IBattleMap, Listener {
     }
 
     public void readyTDMSpawns() {
-        Location redSpawn = new Location(w, 20, 71, -68);
-        Location blueSpawn = new Location(w, 20, 71, 69);
+        Location redSpawn = new Location(w, 20, 71, -68, 179, 0);
+        Location blueSpawn = new Location(w, 20, 71, 69, -1, 0);
         redSpawns.add(redSpawn);
         blueSpawns.add(blueSpawn);
     }
 
     public void readyFFASpawns() {
-        Location redSpawn = new Location(w, 20, 71, -68);
-        Location blueSpawn = new Location(w, 20, 71, 69);
+        Location redSpawn = new Location(w, 20, 71, -68, 179, 0);
+        Location blueSpawn = new Location(w, 20, 71, 69, -1, 0);
         FFASpawns.add(redSpawn);
         FFASpawns.add(blueSpawn);
     }
 
     public void applyInventory(final BattlePlayer p) {
         Inventory i = p.getInventory();
+
+        ItemStack HEALTH_POTION = new ItemStack(Material.POTION, 1, (short) 16373);
+        ItemStack STEAK = new ItemStack(Material.COOKED_BEEF, 1);
+        ItemStack BOW = new ItemStack(Material.BOW, 1);
+        ItemStack ARROWS = new ItemStack(Material.ARROW, 48);
+        ItemStack IRON_CHESTPLATE = new ItemStack(Material.IRON_CHESTPLATE, 1);
+        ItemStack IRON_PANTS = new ItemStack(Material.IRON_LEGGINGS, 1);
+        ItemStack IRON_BOOTS = new ItemStack(Material.IRON_BOOTS, 1);
+        ItemStack DIAMOND_PICKAXE = new ItemStack(Material.DIAMOND_PICKAXE, 1);
         ItemStack ANTIGRAVITY = new ItemStack(Material.DIAMOND, 1);
 
         ItemMeta a = ANTIGRAVITY.getItemMeta();
@@ -67,14 +75,6 @@ public class Distortion extends BattleMap implements IBattleMap, Listener {
         aLore.add(org.bukkit.ChatColor.BLUE + "Hold this to be uninfluenced by the gravity manipulator!");
         a.setLore(aLore);
         ANTIGRAVITY.setItemMeta(a);
-        ItemStack HEALTH_POTION = new ItemStack(Material.POTION, 1, (short) 16373);
-        ItemStack STEAK = new ItemStack(Material.COOKED_BEEF, 1);
-        ItemStack BOW = new ItemStack(Material.BOW, 1);
-        ItemStack ARROWS = new ItemStack(Material.ARROW, 48);
-        ItemStack IRON_CHESTPLATE = new ItemStack(Material.IRON_CHESTPLATE, 1);
-        ItemStack IRON_PANTS = new ItemStack(Material.IRON_LEGGINGS, 1);
-        ItemStack IRON_BOOTS = new ItemStack(Material.IRON_BOOTS, 1);
-        ItemStack DIAMOND_PICKAXE = new ItemStack(Material.DIAMOND_PICKAXE, 1);
 
         p.getInventory().setBoots(IRON_BOOTS);
         p.getInventory().setLeggings(IRON_PANTS);
@@ -86,7 +86,6 @@ public class Distortion extends BattleMap implements IBattleMap, Listener {
         i.setItem(3, HEALTH_POTION);
         i.setItem(8, ANTIGRAVITY);
         i.setItem(28, ARROWS);
-
 
     }
 
@@ -102,23 +101,24 @@ public class Distortion extends BattleMap implements IBattleMap, Listener {
     public int z2 = 128;
 
     int distort;
+
     public void gravityArrows() {
 
         Bukkit.getServer().getScheduler().cancelTask(distort);
         distort = Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
 
             public void run() {
-                if(manipulation == false) return;
+                if (manipulation == false) return;
                 if (getArena().equals(name)) {
                     World world = Bukkit.getWorld("gravity");
-                if (!(world.getEntities() == null)) {
-                    for (Entity arrow : world.getEntities()) {
-                        if (arrow instanceof Arrow) {
-                            arrow.setVelocity(new Vector(arrow.getVelocity().getX(), arrow.getVelocity().getY() - 1, arrow.getVelocity().getZ()));
+                    if (!(world.getEntities() == null)) {
+                        for (Entity arrow : world.getEntities()) {
+                            if (arrow instanceof Arrow) {
+                                arrow.setVelocity(new Vector(arrow.getVelocity().getX(), arrow.getVelocity().getY() - 1, arrow.getVelocity().getZ()));
+                            }
                         }
                     }
                 }
-               }
             }
 
         }, 5L, 5L);
@@ -128,30 +128,30 @@ public class Distortion extends BattleMap implements IBattleMap, Listener {
     public void battleEnd(BattleEndEvent event) {
         Bukkit.getScheduler().cancelTask(distort);
     }
-    
+
     @EventHandler
-    public void manipulator(BlockBreakEvent event){
-        if(!active) return;
-        if(event.getBlock().getType() == Material.SPONGE){
+    public void manipulator(BlockBreakEvent event) {
+        if (!active) return;
+        if (event.getBlock().getType() == Material.SPONGE) {
             event.getBlock().getWorld().getBlockAt(event.getBlock().getLocation()).setType(Material.OBSIDIAN);
             manipulation = false;
             Bukkit.broadcastMessage(ChatColor.AQUA + "The Gravity Manipulator was turned off! Gravity returned to normal!");
             return;
-        } 
-        if(event.getBlock().getType() == Material.OBSIDIAN){
+        }
+        if (event.getBlock().getType() == Material.OBSIDIAN) {
             event.getBlock().getWorld().getBlockAt(event.getBlock().getLocation()).setType(Material.SPONGE);
             manipulation = true;
             Bukkit.broadcastMessage(ChatColor.AQUA + "The Gravity Manipulator was turned on! Gravity intensified!");
         }
     }
-    
+
     @EventHandler
-    public void manipulatorEffect(PlayerMoveEvent event){
+    public void manipulatorEffect(PlayerMoveEvent event) {
         Player p = event.getPlayer();
-        if(manipulation == true){
-            if(p.getItemInHand().getType() != Material.DIAMOND){
-                if(event.getFrom().getY() > event.getTo().getY()){
-                    p.setVelocity(new Vector(0, p.getVelocity().getY() -0.3, 0));
+        if (manipulation == true) {
+            if (p.getItemInHand().getType() != Material.DIAMOND) {
+                if (event.getFrom().getY() > event.getTo().getY()) {
+                    p.setVelocity(new Vector(0, p.getVelocity().getY() - 0.3, 0));
                 }
             }
         }
