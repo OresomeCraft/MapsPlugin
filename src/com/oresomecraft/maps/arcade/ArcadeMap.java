88246e2037f024a138a161a68142e17cc36ebd54
Zach de Koning <zach.de.koning@gmail.com>
2013-11-23 17:47:23 +1100

    Add end() to ArcadeMap

--- src/com/oresomecraft/maps/arcade/ArcadeMap.java
+++ src/com/oresomecraft/maps/arcade/ArcadeMap.java
@@ -1,7 +1,16 @@
 package com.oresomecraft.maps.arcade;
 
+import com.oresomecraft.OresomeBattles.api.events.EndBattleEvent;
 import com.oresomecraft.maps.Map;
+import org.bukkit.Bukkit;
 
 public abstract class ArcadeMap extends Map {
+
+    /**
+     * Ends the battle
+     */
+    public void end() {
+        Bukkit.getPluginManager().callEvent(new EndBattleEvent(getMode()));
+    }
 
 }
