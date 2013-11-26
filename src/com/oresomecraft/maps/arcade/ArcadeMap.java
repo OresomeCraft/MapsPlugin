74cf26cd48857c4e09c1c5c3f7cdb6c9e4868695
R3creat3 <reicyMC@gmail.com>
2013-11-26 20:14:02 +1100

    Add a setTNTAutoIgnite() to ArcadeMap

--- src/com/oresomecraft/maps/arcade/ArcadeMap.java
+++ src/com/oresomecraft/maps/arcade/ArcadeMap.java
@@ -3,7 +3,10 @@ package com.oresomecraft.maps.arcade;
 import com.oresomecraft.OresomeBattles.api.events.EndBattleEvent;
 import com.oresomecraft.maps.Map;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
@@ -12,6 +15,7 @@ public abstract class ArcadeMap extends Map {
 
     private boolean damage = true;
     private boolean explosions = true;
+    private boolean autoTNT = false;
 
 
     /**
@@ -34,10 +38,19 @@ public abstract class ArcadeMap extends Map {
     /**
      * Disables PvP and mob damage
      *
-     * @param check The boolean that defines whether physical damage is allowed
+     * @param check The boolean that defines whether terrain damage is allowed
      */
     public void setAllowTerrainExplosion(boolean check) {
         explosions = check;
+    }
+
+    /**
+     * Disables PvP and mob damage
+     *
+     * @param check The boolean that defines wether tnt is auto-ignited
+     */
+    public void setAutoTNTIgnite(boolean check) {
+        autoTNT = check;
     }
 
 
@@ -62,6 +75,19 @@ public abstract class ArcadeMap extends Map {
     @EventHandler
     public void onExplode(EntityExplodeEvent event) {
         if (event.getEntity().getWorld().getName().equals(name) && !explosions) event.blockList().clear();
+    }
+
+
+    /**
+     * Auto ignites TNT if defined
+     *
+     * @param event an Event called by bukkit
+     */
+    @EventHandler
+    public void onExplode(BlockPlaceEvent event) {
+        if (!event.getBlock().getWorld().getName().equals(name)) return;
+        event.getBlock().setType(Material.AIR);
+        event.getBlock().getWorld().spawnEntity(event.getBlock().getLocation(), EntityType.PRIMED_TNT);
     }
 
 }
