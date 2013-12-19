0881d31b4e523428ed961b15ee6b15af60dcf298
Huskehhh <ttghusky@live.com.au>
2013-12-19 17:37:37 +1100

    Remove notes

--- src/com/oresomecraft/maps/arcade/maps/DynaBlast.java
+++ src/com/oresomecraft/maps/arcade/maps/DynaBlast.java
@@ -22,20 +22,6 @@ public class DynaBlast extends ArcadeMap implements Listener {
     String creators = "Htgan";
     Gamemode[] modes = {Gamemode.LMS};
 
-    /**
-     * public void readyTDMSpawns() {
-     * redSpawns.add(new Location(w, -29, 65, -29, 0, 0));
-     * redSpawns.add(new Location(w, 11, 65, -29, 0, 0));
-     * redSpawns.add(new Location(w, -29, 65, 11, 0, 0));
-     * redSpawns.add(new Location(w, 11, 65, 11, 0, 0));
-     * <p/>
-     * blueSpawns.add(new Location(w, 1, 65, 1, 0, 0));
-     * blueSpawns.add(new Location(w, -19, 65, 1, 0, 0));
-     * blueSpawns.add(new Location(w, -19, 65, -19, 0, 0));
-     * blueSpawns.add(new Location(w, 1, 65, -19, 0, 0));
-     * }
-     */
-
     public void readyFFASpawns() {
         FFASpawns.add(new Location(w, -29, 65, -29, 0, 0));
         FFASpawns.add(new Location(w, 11, 65, -29, 0, 0));
