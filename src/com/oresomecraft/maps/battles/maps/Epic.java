71769892ec73714bc03dfaf165b5a1926ed8dc30
ScruffyRules <scott-scheiner@hotmail.com>
2014-01-01 20:40:21 +1030

    I

--- src/com/oresomecraft/maps/battles/maps/Epic.java
+++ src/com/oresomecraft/maps/battles/maps/Epic.java
@@ -40,22 +40,22 @@ public class Epic extends BattleMap implements IBattleMap, Listener {
  
         ItemStack LEATHER_HELMET = new ItemStack(Material.LEATHER_HELMET, 1);
         ItemStack LEATHER_CHESTPLATE = new ItemStack(Material.LEATHER_CHESTPLATE, 1);
-        ItemStack LEATHER_PANTS = new ItemStack(Material.LEATHER_PANTS, 1);
+        ItemStack LEATHER_LEGGINGS = new ItemStack(Material.LEATHER_LEGGINGS, 1);
         ItemStack LEATHER_BOOTS = new ItemStack(Material.LEATHER_BOOTS, 1);
-        ItemStack WOODEN_SWORD = new ItemStack(Material.WOODEN_SWORD, 1);
+        ItemStack WOOD_SWORD = new ItemStack(Material.WOOD_SWORD, 1);
         ItemStack STEAK = new ItemStack(Material.COOKED_BEEF, 1);
         ItemStack GOLDEN_APPLE = new ItemStack(Material.GOLDEN_APPLE, 1);
         ItemStack EXP_BOTTLE = new ItemStack(Material.EXP_BOTTLE, 1);
         
-        InvUtils.colourArmourAccordingToTeam(p, new ItemStack[]{LEATHER_HELMET, LEATHER_CHESTPLATE, LEATHER_PANTS, LEATHER_BOOTS});
+        InvUtils.colourArmourAccordingToTeam(p, new ItemStack[]{LEATHER_HELMET, LEATHER_CHESTPLATE, LEATHER_LEGGINGS, LEATHER_BOOTS});
         
         p.getInventory().setBoots(LEATHER_BOOTS);
-        p.getInventory().setLeggings(LEATHER_PANTS);
+        p.getInventory().setLeggings(LEATHER_LEGGINGS);
         p.getInventory().setChestplate(LEATHER_CHESTPLATE);
         p.getInventory().setHelmet(LEATHER_HELMET);
  
         // setItem() is a BattlePlayer method. Makes giving items a bit quicker.
-        p.setItem(0, WOODEN_SWORD);
+        p.setItem(0, WOOD_SWORD);
         p.setItem(1, GOLDEN_APPLE, 3);
         p.setItem(2, STEAK, 4);
         p.setItem(3, EXP_BOTTLE, 3);
