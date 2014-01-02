c9c3e8d3c503a109de4a27158ca75aa6f70800d1
AnomalousRei <anomalousrei@gmail.com>
2014-01-02 13:21:26 +1100

    3 steak for terminal

--- src/com/oresomecraft/maps/battles/maps/Terminal.java
+++ src/com/oresomecraft/maps/battles/maps/Terminal.java
@@ -60,7 +60,7 @@ public class Terminal extends BattleMap implements IBattleMap, Listener {
         Inventory i = p.getInventory();
 
         ItemStack HEALTH_POTION = new ItemStack(Material.POTION, 1, (short) 16373);
-        ItemStack STEAK = new ItemStack(Material.COOKED_BEEF, 1);
+        ItemStack STEAK = new ItemStack(Material.COOKED_BEEF, 3);
         ItemStack BOW = new ItemStack(Material.BOW, 1);
         ItemStack ARROWS = new ItemStack(Material.ARROW, 64);
         ItemStack IRON_HELMET = new ItemStack(Material.IRON_HELMET, 1);
