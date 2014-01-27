960ef4b9bded400bf9fbd223b6b70c23f1708e6f
Zach de Koning <zach.de.koning@gmail.com>
2014-01-28 07:38:19 +1100

    Add Trouble in Oresome Town support

--- src/com/oresomecraft/maps/battles/maps/PrototypeTown.java
+++ src/com/oresomecraft/maps/battles/maps/PrototypeTown.java
@@ -20,10 +20,10 @@ public class PrototypeTown extends BattleMap implements IBattleMap, Listener {
         disableDrops(new Material[]{Material.LEATHER_HELMET, Material.STONE_SWORD});
     }
 
-    String name = "prototypetown";
+    String name = "ninduck";
     String fullName = "Prototype Town";
     String creators = "ninsai, SuperDuckFace, beadycottonwood and XUHAVON";
-    Gamemode[] modes = {Gamemode.TDM, Gamemode.CTF, Gamemode.KOTH};
+    Gamemode[] modes = {Gamemode.TDM, Gamemode.CTF, Gamemode.KOTH, Gamemode.TIOT};
 
     public void readyTDMSpawns() {
         redSpawns.add(new Location(w, -160, 65, -156));
@@ -42,42 +42,10 @@ public class PrototypeTown extends BattleMap implements IBattleMap, Listener {
     }
 
     public void readyFFASpawns() {
-        FFASpawns.add(new Location(w, 2, 84, -48));
-        FFASpawns.add(new Location(w, -3, 84, 58));
+        FFASpawns.add(new Location(w, -126, 66, -108));
     }
 
     public void applyInventory(final BattlePlayer p) {
-        Inventory i = p.getInventory();
-
-        ItemStack LEATHER_HELMET = new ItemStack(Material.LEATHER_HELMET, 1);
-        ItemStack LEATHER_CHESTPLATE = new ItemStack(Material.LEATHER_CHESTPLATE, 1);
-        ItemStack LEATHER_PANTS = new ItemStack(Material.LEATHER_LEGGINGS, 1);
-        ItemStack LEATHER_BOOTS = new ItemStack(Material.LEATHER_BOOTS, 1);
-        ItemStack SWORD = new ItemStack(Material.STONE_SWORD, 1);
-        ItemStack BOW = new ItemStack(Material.BOW, 1);
-        ItemStack STEAK = new ItemStack(Material.COOKED_BEEF, 3);
-        ItemStack EXP = new ItemStack(Material.EXP_BOTTLE, 5);
-        ItemStack HEALTH = new ItemStack(Material.GOLDEN_APPLE, 3);
-        ItemStack ARROWS = new ItemStack(Material.ARROW, 64);
-        ItemStack ARROWS2 = new ItemStack(Material.ARROW, 64);
-
-
-        InvUtils.colourArmourAccordingToTeam(p, new ItemStack[]{LEATHER_CHESTPLATE, LEATHER_PANTS, LEATHER_HELMET, LEATHER_BOOTS});
-
-        p.getInventory().setBoots(LEATHER_BOOTS);
-        p.getInventory().setLeggings(LEATHER_PANTS);
-        p.getInventory().setChestplate(LEATHER_CHESTPLATE);
-        p.getInventory().setHelmet(LEATHER_HELMET);
-
-        i.setItem(0, SWORD);
-        i.setItem(1, BOW);
-        i.setItem(2, STEAK);
-        i.setItem(3, HEALTH);
-        i.setItem(4, EXP);
-        i.setItem(10, ARROWS);
-        i.setItem(11, ARROWS2);
-
-        p.addPotionEffect(new PotionEffect(PotionEffectType.HEAL, 5 * 20, 2));
 
     }
 
