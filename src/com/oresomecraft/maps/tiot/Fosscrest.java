7e87057cccf444a394ebcc820e359a00383938d2
AnomRei <anomalousrei@gmail.com>
2014-02-01 16:51:46 +1100

    Add Fosscrest for TiOT

---
+++ src/com/oresomecraft/maps/tiot/Fosscrest.java
@@ -0,0 +1,44 @@
+package com.oresomecraft.maps.tiot;
+
+import com.oresomecraft.OresomeBattles.api.BattlePlayer;
+import com.oresomecraft.OresomeBattles.api.CuboidRegion;
+import com.oresomecraft.OresomeBattles.api.Gamemode;
+import com.oresomecraft.maps.MapConfig;
+import com.oresomecraft.maps.battles.BattleMap;
+import com.oresomecraft.maps.battles.IBattleMap;
+import org.bukkit.Location;
+import org.bukkit.event.Listener;
+
+@MapConfig
+public class Fosscrest extends BattleMap implements IBattleMap, Listener {
+
+    public Fosscrest() {
+        super.initiate(this, name, fullName, creators, modes);
+        setAllowBuild(false);
+    }
+
+    // Map details
+    String name = "fosscrest";
+    String fullName = "Fosscrest Village";
+    String creators = "__R3, danielschroeder and xXJazzerXx";
+    Gamemode[] modes = {Gamemode.TIOT};
+
+    public void readyFFASpawns() {
+        FFASpawns.add(new Location(w, 231, 103, -1292));
+        FFASpawns.add(new Location(w, 234, 101, -1308));
+        FFASpawns.add(new Location(w, 206, 107, -1287));
+        FFASpawns.add(new Location(w, 203, 111, -1269));
+        FFASpawns.add(new Location(w, 173, 119, -1268));
+
+        setCriminalTester(new CuboidRegion(new Location(w, 189, 111, -1298), new Location(w, 185, 119, -1303)));
+    }
+
+    public void readyTDMSpawns() {
+        // No TDM spawns
+    }
+
+    public void applyInventory(final BattlePlayer p) {
+        // No predefined inventory
+    }
+
+}
