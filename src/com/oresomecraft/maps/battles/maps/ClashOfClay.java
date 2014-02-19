3493258ea0e73d8719e33b34c3068fac91170749
ScruffyRules <scott-scheiner@hotmail.com>
2014-02-19 17:25:36 +1030

    20 minutes is way too long

--- src/com/oresomecraft/maps/battles/maps/ClashOfClay.java
+++ src/com/oresomecraft/maps/battles/maps/ClashOfClay.java
@@ -16,7 +16,7 @@ public class ClashOfClay extends BattleMap implements IBattleMap, Listener {
 
     public ClashOfClay() {
         super.initiate(this, name, fullName, creators, modes);
-        setTDMTime(20);
+        setTDMTime(10);
         disableDrops(new Material[]{Material.DIAMOND_HELMET, Material.WOOD_SWORD});
     }
 
