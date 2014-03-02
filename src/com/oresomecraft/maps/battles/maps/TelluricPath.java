0d64931e4cd43e43fbfd738f72f841aefd3dfdc4
SuperDuckFace <hartleyjr11@gmail.com>
2014-03-02 17:07:38 +1000

    Remove LTS and Infection
    Removing LTS because the games go for too long and loses the player count.
    Same for infection.

--- src/com/oresomecraft/maps/battles/maps/TelluricPath.java
+++ src/com/oresomecraft/maps/battles/maps/TelluricPath.java
@@ -34,7 +34,7 @@ public class TelluricPath extends BattleMap implements IBattleMap, Listener {
     String name = "telluricpath";
     String fullName = "Telluric Path";
     String creators = "__R3 ";
-    Gamemode[] modes = {Gamemode.CTF, Gamemode.LTS, Gamemode.INFECTION};
+    Gamemode[] modes = {Gamemode.CTF};
 
     public void readyTDMSpawns() {
 
