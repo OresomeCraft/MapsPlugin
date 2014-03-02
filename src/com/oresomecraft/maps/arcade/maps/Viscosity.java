2da4d204a9b9e98d581ee766ebd9066a88267ece
AnomalousRei <anomalousrei@gmail.com>
2014-03-02 16:33:17 +1100

    Revert "remove my unused maps"
    This reverts commit 1949d74150759431aa683bb083372eb5ee047e20.

---
+++ src/com/oresomecraft/maps/arcade/maps/Viscosity.java
@@ -0,0 +1,43 @@
+package com.oresomecraft.maps.arcade.maps;
+
+import com.oresomecraft.OresomeBattles.api.BattlePlayer;
+import com.oresomecraft.OresomeBattles.api.Gamemode;
+import com.oresomecraft.maps.MapConfig;
+import com.oresomecraft.maps.arcade.games.TNTRunMap;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+@MapConfig
+public class Viscosity extends TNTRunMap implements Listener {
+
+    public Viscosity() {
+        super.initiate(this, name, fullName, creators, modes);
+        disableDrops(new Material[]{Material.COOKED_BEEF});
+        setAllowPhysicalDamage(false);
+        setAllowBuild(false);
+    }
+
+    // Map details
+    String name = "viscosity";
+    String fullName = "Viscosity Speed Run";
+    String creators = "Rynocraft, BlueVortexed and AnomalousRei ";
+    Gamemode[] modes = {Gamemode.LMS};
+
+    public void readyFFASpawns() {
+        FFASpawns.add(new Location(w, -29, 74, -28));
+    }
+
+    public void applyInventory(final BattlePlayer p) {
+        Inventory i = p.getInventory();
+
+        ItemStack STEAK = new ItemStack(Material.COOKED_BEEF, 3);
+        i.setItem(0, STEAK);
+
+        p.sendMessage(ChatColor.RED + "RUN!!!");
+    }
+
+}
