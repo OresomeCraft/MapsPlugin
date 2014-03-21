16e999399deffae7b111c45883dc74da52b69511
AnomalousRei <anomalousrei@gmail.com>
2014-03-21 20:41:21 +1100

    Add Rainbow Road Power Build

---
+++ src/com/oresomecraft/maps/oresomekart/maps/Rainbow.java
@@ -0,0 +1,42 @@
+package com.oresomecraft.maps.oresomekart.maps;
+
+import com.oresomecraft.OresomeBattles.api.BattlePlayer;
+import com.oresomecraft.OresomeBattles.api.CuboidRegion;
+import com.oresomecraft.OresomeBattles.api.Gamemode;
+import com.oresomecraft.maps.MapConfig;
+import com.oresomecraft.maps.oresomekart.OresomeKartMap;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.event.Listener;
+
+@MapConfig
+public class Rainbow extends OresomeKartMap implements Listener {
+
+    public Rainbow() {
+        super.initiate(this, name, fullName, creators, modes);
+        setAllowBuild(false);
+        setFinishPoint(new CuboidRegion(new Location(w, -1, 82, -3), new Location(w, 0, 65, 15)));
+        setHalfwayCheckPoint(new CuboidRegion(new Location(w, 20, 65, -37), new Location(w, 19, 45, -54)));
+        setLaps(3);
+        setDrivableSurfaces(new Material[]{Material.WOOL, Material.GOLD_BLOCK});
+        setBoostSurfaces(new Material[]{Material.SANDSTONE, Material.DIAMOND_BLOCK});
+        setJumpSurfaces(new Material[]{Material.DIAMOND_BLOCK});
+
+        setFireSpread(false);
+    }
+
+    // Map details
+    String name = "rainbow_ok";
+    String fullName = "Rainbow Road Power Build";
+    String creators = "__R3, reggie449 and WiiiFreak123";
+    Gamemode[] modes = {Gamemode.ORESOMEKART};
+
+    public void readyFFASpawns() {
+        FFASpawns.add(new Location(w, -3, 70, 6));
+    }
+
+    public void applyInventory(final BattlePlayer p) {
+        // No items needed
+    }
+
+}
