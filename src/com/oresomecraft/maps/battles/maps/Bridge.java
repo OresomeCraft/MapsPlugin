7e02e19164e32dc3f9914b79f43133e3882060bf
AnomalousRei <anomalousrei@gmail.com>
2014-04-04 22:13:05 +1100

    Upgrade Bridge to Bridge 2.0, removing certain items, adding CTF and changing the kit.

--- src/com/oresomecraft/maps/battles/maps/Bridge.java
+++ src/com/oresomecraft/maps/battles/maps/Bridge.java
@@ -14,7 +14,7 @@ public class Bridge extends BattleMap implements IBattleMap, Listener {
     public Bridge() {
         super.initiate(this, name, fullName, creators, modes);
         setAllowBuild(false);
-        setTDMTime(10);
+        setTDMTime(15);
         disableDrops(new Material[]{Material.LEATHER_HELMET, Material.BOW});
         setAutoSpawnProtection(15);
     }
@@ -22,14 +22,15 @@ public class Bridge extends BattleMap implements IBattleMap, Listener {
     // Map details
     String name = "bridge";
     String fullName = "Bridge";
-    String creators = "fkBear, ep1cn00bt00b and FLOPPYCABBAGE";
-    Gamemode[] modes = {Gamemode.TDM, Gamemode.KOTH};
+    String creators = "fkBear and ep1cn00bt00b";
+    Gamemode[] modes = {Gamemode.TDM, Gamemode.KOTH, Gamemode.CTF};
 
     public void readyTDMSpawns() {
-        redSpawns.add(new Location(w, -14, 66, -193, 0, 0));
-        blueSpawns.add(new Location(w, -15, 66, -17, 180, 0));
+        redSpawns.add(new Location(w, -4, 64, 8, 317.4F, 14F));
+        blueSpawns.add(new Location(w, -27, 64, -218, 144.2F, 1.1F));
 
-        setKoTHMonument(new Location(w, -16, 67, -106));
+        setKoTHMonument(new Location(w, -15, 67, -105));
+        setCTFFlags(name, new Location(w, -16, 57, -52), new Location(w, -15, 67, -158));
     }
 
     public void readyFFASpawns() {
@@ -41,25 +42,25 @@ public class Bridge extends BattleMap implements IBattleMap, Listener {
         Inventory i = p.getInventory();
 
         ItemStack LEATHER_HELMET = new ItemStack(Material.LEATHER_HELMET, 1);
-        ItemStack LEATHER_CHESTPLATE = new ItemStack(Material.LEATHER_CHESTPLATE, 1);
+        ItemStack IRON_CHESTPLATE = new ItemStack(Material.IRON_CHESTPLATE, 1);
         ItemStack LEATHER_LEGGINGS = new ItemStack(Material.LEATHER_LEGGINGS, 1);
         ItemStack LEATHER_BOOTS = new ItemStack(Material.LEATHER_BOOTS, 1);
-        ItemStack WOOD_SWORD = new ItemStack(Material.WOOD_SWORD, 1);
+        ItemStack STONE_SWORD = new ItemStack(Material.STONE_SWORD, 1);
         ItemStack STEAK = new ItemStack(Material.COOKED_BEEF, 3);
         ItemStack GOLDEN_APPLE = new ItemStack(Material.GOLDEN_APPLE, 1);
         ItemStack EXP_BOTTLE = new ItemStack(Material.EXP_BOTTLE, 3);
         ItemStack BOW = new ItemStack(Material.BOW, 1);
         ItemStack ARROWS = new ItemStack(Material.ARROW, 16);
 
-        InvUtils.colourArmourAccordingToTeam(p, new ItemStack[]{LEATHER_HELMET, LEATHER_CHESTPLATE, LEATHER_LEGGINGS, LEATHER_BOOTS});
+        InvUtils.colourArmourAccordingToTeam(p, new ItemStack[]{LEATHER_HELMET, LEATHER_LEGGINGS, LEATHER_BOOTS});
 
         p.getInventory().setBoots(LEATHER_BOOTS);
         p.getInventory().setLeggings(LEATHER_LEGGINGS);
-        p.getInventory().setChestplate(LEATHER_CHESTPLATE);
+        p.getInventory().setChestplate(IRON_CHESTPLATE);
         p.getInventory().setHelmet(LEATHER_HELMET);
 
         // setItem() is a BattlePlayer method. Makes giving items a bit quicker.
-        i.setItem(0, WOOD_SWORD);
+        i.setItem(0, STONE_SWORD);
         i.setItem(2, GOLDEN_APPLE);
         i.setItem(1, BOW);
         i.setItem(9, ARROWS);
@@ -67,8 +68,8 @@ public class Bridge extends BattleMap implements IBattleMap, Listener {
         i.setItem(4, EXP_BOTTLE);
     }
 
-    // Region. (Top corner block and bottom corner block.
-    // Top left corner.
+    //Unneeded.
+    /*
     public int x1 = -67;
     public int y1 = 40;
     public int z1 = 15;
@@ -77,5 +78,5 @@ public class Bridge extends BattleMap implements IBattleMap, Listener {
     public int x2 = 36;
     public int y2 = 101;
     public int z2 = -223;
-
+    */
 }
