6e6e4cb19bff3d96530baf78be19139c611d46df
AnomalousRei <anomalousrei@gmail.com>
2014-04-10 16:09:15 +1000

    Add sexy water-walking rose to Oasis

--- src/com/oresomecraft/maps/battles/maps/Oasis.java
+++ src/com/oresomecraft/maps/battles/maps/Oasis.java
@@ -16,9 +16,14 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @MapConfig
 public class Oasis extends BattleMap implements IBattleMap, Listener {
@@ -65,12 +70,13 @@ public class Oasis extends BattleMap implements IBattleMap, Listener {
         ItemStack BOW = new ItemStack(Material.BOW, 1);
         ItemStack ARROWS = new ItemStack(Material.ARROW, 16);
         ItemStack STONE_SWORD = new ItemStack(Material.STONE_SWORD, 1);
+        ItemStack ROSE = new ItemStack(Material.RED_ROSE, 1, (short) 1);
 
         ItemStack LEATHER_HELMET = new ItemStack(Material.LEATHER_HELMET, 1);
         ItemStack IRON_CHESTPLATE = new ItemStack(Material.IRON_CHESTPLATE, 1);
         ItemStack LEATHER_PANTS = new ItemStack(Material.LEATHER_LEGGINGS, 1);
         ItemStack LEATHER_BOOTS = new ItemStack(Material.LEATHER_BOOTS, 1);
-;
+        ;
         InvUtils.colourArmourAccordingToTeam(p, new ItemStack[]{LEATHER_PANTS, LEATHER_HELMET, LEATHER_BOOTS});
 
         p.getInventory().setBoots(LEATHER_BOOTS);
@@ -82,7 +88,50 @@ public class Oasis extends BattleMap implements IBattleMap, Listener {
         i.setItem(1, BOW);
         i.setItem(3, STEAK);
         i.setItem(4, HEALTH);
+        i.setItem(8, ROSE);
         i.setItem(10, ARROWS);
 
+    }
+
+    public void iceTemp(final Block block, final Player p) {
+        block.setType(Material.PACKED_ICE);
+        new BukkitRunnable() {
+            public void run() {
+                block.setType(Material.STATIONARY_WATER);
+                if (block.getLocation().distanceSquared(p.getLocation()) <= 3) iceTemp(block, p);
+            }
+        }.runTaskLater(plugin, 3 * 20L);
+    }
+
+    @EventHandler
+    public void move(PlayerMoveEvent event) {
+        if (!event.getPlayer().getWorld().getName().equals(name)) return;
+        if (!(event.getPlayer().getItemInHand().getType() == Material.RED_ROSE)) return;
+        for (Block block : circle(event.getPlayer().getLocation(), 3, 3, false, true, 0)) {
+            if (block.getType() == Material.WATER || block.getType() == Material.STATIONARY_WATER) {
+                iceTemp(block, event.getPlayer());
+            }
+        }
+    }
+
+    protected static List<Block> circle(Location loc, int radius, int height, boolean hollow, boolean sphere, int plusY) {
+        List<Block> circleblocks = new ArrayList<Block>();
+        int cx = loc.getBlockX();
+        int cy = loc.getBlockY();
+        int cz = loc.getBlockZ();
+
+        for (int x = cx - radius; x <= cx + radius; x++) {
+            for (int z = cz - radius; z <= cz + radius; z++) {
+                for (int y = (sphere ? cy - radius : cy); y < (sphere ? cy + radius : cy + height); y++) {
+                    double dist = (cx - x) * (cx - x) + (cz - z) * (cz - z) + (sphere ? (cy - y) * (cy - y) : 0);
+                    if (dist < radius * radius && !(hollow && dist < (radius - 1) * (radius - 1))) {
+                        Location l = new Location(loc.getWorld(), x, y + plusY, z);
+                        circleblocks.add(l.getBlock());
+                    }
+                }
+            }
+        }
+
+        return circleblocks;
     }
 }
