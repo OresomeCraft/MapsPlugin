fea091ebf2e87a224d83f35bb004af587c5984cb
psgs <fpigerre@gmail.com>
2014-04-28 20:53:04 +1000

    Add OrgeIsland, Update TiOT Template
    Add OrgeIsland by miniwolf35 and SereneMango, configured by
    @ScruffyRules. Update TioT Template.

---
+++ src/com/oresomecraft/maps/tiot/maps/OrgeIsland.java
@@ -0,0 +1,51 @@
+package com.oresomecraft.maps.tiot.maps;
+ 
+import com.oresomecraft.OresomeBattles.api.BattlePlayer;
+import com.oresomecraft.OresomeBattles.api.CuboidRegion;
+import com.oresomecraft.OresomeBattles.api.Gamemode;
+import com.oresomecraft.OresomeBattles.api.Team;
+import com.oresomecraft.maps.MapConfig;
+import com.oresomecraft.maps.tiot.TiOTMap;
+import org.bukkit.ChatColor;
+import org.bukkit.Color;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
+ 
+@MapConfig
+public class OrgeIsland extends TiOTMap implements Listener {
+ 
+    public OrgeIsland() {
+        super.initiate(this, name, fullName, creators, modes);
+        setAllowBuild(false);
+    }
+ 
+    // Map details
+    String name = "orgeisland";
+    String fullName = "Orge Island";
+    String creators = "miniwolf35 and SereneMango ";
+    Gamemode[] modes = {Gamemode.TIOT};
+ 
+    public void readyFFASpawns() {
+ 
+        FFASpawns.add(new Location(w, 40, 69, -9, 90, 0));
+        FFASpawns.add(new Location(w, -24, 67, 6, -90, 0));
+        FFASpawns.add(new Location(w, -26, 67, -27, 0, 0));
+ 
+        setCriminalTester(new CuboidRegion(new Location(w, 14, 66, 3), new Location(w, 10, 70, 0)));
+    }
+ 
+    public void applyInventory(final BattlePlayer p) {
+        if (p.getTeamType() == Team.TIOT_INVESTIGATORS) {
+            ItemStack INVESTIGATOR_HAT = new ItemStack(Material.LEATHER_HELMET, 1);
+            LeatherArmorMeta hatMeta = (LeatherArmorMeta) INVESTIGATOR_HAT.getItemMeta();
+            hatMeta.setDisplayName(ChatColor.LIGHT_PURPLE + "Investigator's Hat");
+            hatMeta.setColor(Color.PURPLE);
+            INVESTIGATOR_HAT.setItemMeta(hatMeta);
+            p.getInventory().setHelmet(INVESTIGATOR_HAT);
+        }
+    }
+ 
+}
