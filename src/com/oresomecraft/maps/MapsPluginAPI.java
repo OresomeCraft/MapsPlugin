730c60c547443f66e4918c90dadc2d32175bbfae
__R3 <anomalousrei@gmail.com>
2014-05-23 17:03:57 +1000

    Make the API directly grab the Map attribute from it's object instead of using HashMaps

--- src/com/oresomecraft/maps/MapsPluginAPI.java
+++ src/com/oresomecraft/maps/MapsPluginAPI.java
@@ -1,33 +1,6 @@
 package com.oresomecraft.maps;
 
-import java.util.HashMap;
-
 public class MapsPluginAPI {
-
-    private static HashMap<String, String> nameToFull = new HashMap<String, String>();
-    private static HashMap<String, String[]> nameToCreators = new HashMap<String, String[]>();
-
-    /**
-     * Registers a Map's full name and short name into a HashMap.
-     *
-     * @param shortName The short name to add.
-     * @param fullName  The full name to be identified at a later time.
-     */
-    public static void putFullName(String shortName, String fullName) {
-        if (!nameToFull.containsValue(fullName)) return;
-        nameToFull.put(shortName, fullName);
-    }
-
-    /**
-     * Registers a Map's creator list into a HashMap.
-     *
-     * @param shortName The short name to add.
-     * @param creators  The creator list to be identified at a later time.
-     */
-    public static void putCreators(String shortName, String[] creators) {
-        if (!nameToCreators.containsValue(creators)) return;
-        nameToCreators.put(shortName, creators);
-    }
 
     /**
      * Resolves a short name to a full map name (i.e. perro -> Casa de Perro)
@@ -36,8 +9,8 @@ public class MapsPluginAPI {
      * @return The resolved name, returns 'None' if it couldn't resolve.
      */
     public static String resolveShortName(String shortName) {
-        String resolve = nameToFull.get(shortName);
-        if (!nameToFull.containsKey(shortName)) return "None";
+        String resolve = MapsPlugin.getMaps().get(shortName).getFullName();
+        if (resolve == null) return "None";
         return resolve;
     }
 
@@ -48,8 +21,8 @@ public class MapsPluginAPI {
      * @return The resolved creator list, returns null if it couldn't resolve.
      */
     public static String[] resolveCreators(String shortName) {
-        String[] resolve = nameToCreators.get(shortName);
-        if (!nameToCreators.containsKey(shortName)) return null;
+        String[] resolve = MapsPlugin.getMaps().get(shortName).getCreators();
+        if (resolve == null) return new String[]{"???"};
         return resolve;
     }
 }
