06ecdd258d5ab0dbfc6cc03310488dec5fc80fb8
PyroPolar <pyropolarmc@gmail.com>
2014-06-22 19:17:54 +1000

    Update BattleTemplate.java

--- templates/BattleTemplate.java
+++ templates/BattleTemplate.java
@@ -8,25 +8,24 @@ import com.oresomecraft.OresomeBattles.map.annotations.*;
 import com.oresomecraft.OresomeBattles.map.types.BattleMap;
 
 @MapConfig(
-        name = "candycavern",
-        fullName = "Candy Cavern",
-        creators = {"PyroPolar},
-        gamemodes = {Gamemode.INFECTION, Gamemode.FFA, Gamemode.LTS, Gamemode.LMS}
-        //http://www.mediafire.com/download/ods1iuoh78bb2ng/Candy_Cavern.zip
+        name = "template",
+        fullName = "Template",
+        creators = {"zachoz", "ScruffyRules"},
+        gamemodes = {Gamemode.TDM, Gamemode.FFA, Gamemode.CTF, Gamemode.KOTH, Gamemode.LTS, Gamemode.LMS}
 )
 @Region(
-        x1 = -301,
-        y1 = 179,
-        z1 = 63,
-        x2 = -379,
-        y2 = 127,
-        z2 = 143
+        x1 = 0,
+        y1 = 0,
+        z1 = 0,
+        x2 = 0,
+        y2 = 0,
+        z2 = 0
 )
 @Attributes(
         allowBuild = false,
         fireSpread = false,
         tdmTime = 10,
-        disabledDrops = {Material.BOW, Material.IRON_SWORD, Material.IRON_HELMET, Material.IRON_LEGGINGS, Material.IRON_BOOTS, Material.LEATHER_CHESTPLATE}
+        disabledDrops = {Material.BOW, Material.IRON_SWORD}
 )
 public class BattleTemplate extends BattleMap {
 
@@ -35,21 +34,20 @@ public class BattleTemplate extends BattleMap {
     }
 
     public void readyTDMSpawns() {
-        redSpawns.add(new Location(w, -854, 156, 78, 1, 0));
+        redSpawns.add(new Location(w, 0, 99, 27, 2, 0));
+        redSpawns.add(new Location(w, -9, 110, -20, 0, 0));
+        redSpawns.add(new Location(w, 21, 105, -13, 0, 0));
+        redSpawns.add(new Location(w, 4, 106, -41, 0, 0));
 
-        blueSpawns.add(new Location(w, -825, 150, 128, 179, 0));
-        
+        blueSpawns.add(new Location(w, -9, 110, -20, 0, 0));
+        blueSpawns.add(new Location(w, 0, 99, 27, 0, 0));
+        blueSpawns.add(new Location(w, -16, 108, -3, 0, 0));
+        blueSpawns.add(new Location(w, -30, 108, -3, 0, 0));
     }
 
     public void readyFFASpawns() {
-        FFASpawns.add(new Location(w, -854, 156, 78, 1, 0));
-        FFASpawns.add(new Location(w, -825, 150, 128, 179, 0));
-        FFASpawns.add(new Location(w, -838, 139, 130, 179, 0));
-        FFASpawns.add(new Location(w, -835, 140, 122, 90, 0));
-        FFASpawns.add(new Location(w, -838, 142, 84, 1, 0));
-        FFASpawns.add(new Location(w, -842, 139, 76, 1, 0));
-        FFASpawns.add(new Location(w, -872, 135, 100, -90, 0));
-        FFASpawns.add(new Location(w, -810, 137, 95, 90, 0));
+        FFASpawns.add(new Location(w, 0, 99, 27, 2, 0));
+        FFASpawns.add(new Location(w, -9, 110, -20, 0, 0));
     }
 
     public void applyInventory(final BattlePlayer p) {
@@ -57,25 +55,16 @@ public class BattleTemplate extends BattleMap {
 
         ItemStack HEALTH_POTION = new ItemStack(Material.POTION, 1, (short) 16373);
         ItemStack IRON_HELMET = new ItemStack(Material.IRON_HELMET, 1);
-        ItemStack LEATHER_CHESTPLATE = new ItemStack(Material.;LEATHER_CHESTPLATE, 1);
+        ItemStack IRON_CHESTPLATE = new ItemStack(Material.IRON_CHESTPLATE, 1);
         ItemStack IRON_PANTS = new ItemStack(Material.IRON_LEGGINGS, 1);
         ItemStack IRON_BOOTS = new ItemStack(Material.IRON_BOOTS, 1);
         ItemStack IRON_SWORD = new ItemStack(Material.IRON_SWORD, 1);
-        ItemStack BOW = new ItemStack(Material.BOW, 1);
-
-        InvUtils.colourArmourAccordingToTeam(p, new ItemStack[]{LEATHER_CHESTPLATE});
 
         p.getInventory().setBoots(IRON_BOOTS);
         p.getInventory().setLeggings(IRON_PANTS);
-        p.getInventory().setChestplate(LEATHER_CHESTPLATE);
+        p.getInventory().setChestplate(IRON_CHESTPLATE);
         p.getInventory().setHelmet(IRON_HELMET);
 
-        InvUtils.nameItem(IRON_SWORD, ChatColor.WHITE + "Candy Cruncher");
-        InvUtils.nameItem(Material.BOW, ChatColor.WHITE + "Lolly Lasher");
-        InvUtils.nameItem(Material.COOKED_BEEF, ChatColor.WHITE + "Licorice");
-        InvUtils.nameItem(HEALTH_POTION, ChatColor.WHITE + "Soda");
-        InvUtils.nameItem(Material.ARROW, ChatColor.WHITE + "Gobstopper");
-        
         // setItem() is a BattlePlayer method. Makes giving items a bit quicker.
         p.setItem(0, IRON_SWORD);
         p.setItem(1, Material.BOW, 1);
@@ -83,6 +72,7 @@ public class BattleTemplate extends BattleMap {
 
         // This is the Bukkit way of doing it
         i.setItem(3, HEALTH_POTION);
+        p.setItem(8, Material.EXP_BOTTLE, 5);
         p.setItem(9, Material.ARROW, 64);
     }
 
